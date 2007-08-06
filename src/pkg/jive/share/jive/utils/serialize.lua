a4cf71662597246f5b78e6711825a4c48c728b28
Ben Klaas <bklaas@logitech.com>
2007-08-06 21:20:38 +0000

    Add semicolon at the end of all statements Add support for storing boolean values
    git-svn-id: https://svn.slimdevices.com/repos/jive/trunk/jive@216 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/pkg/jive/share/jive/utils/serialize.lua
+++ src/pkg/jive/share/jive/utils/serialize.lua
@@ -20,13 +20,18 @@ local string = require("string")
 
 local pairs, tostring, type = pairs, tostring, type
 
-
 module(...)
 
 
 function basicSerialize (o)
 	if type(o) == "number" then
 		return tostring(o)
+	elseif type(o) == "boolean" then
+		if o == true then
+			return "true"
+		else
+			return "false"
+		end
 	else   -- assume it is a string
 		return string.format("%q", o)
 	end
@@ -37,19 +42,21 @@ function save (file, name, value, saved)
 	saved = saved or {}       -- initial value
 	file:write(name, " = ")
 	if type(value) == "number" or type(value) == "string" then
-		file:write(basicSerialize(value), "\n")
+		file:write(basicSerialize(value), ";\n")
 	elseif type(value) == "table" then
 		if saved[value] then    -- value already saved?
-			file:write(saved[value], "\n")  -- use its previous name
+			file:write(saved[value], ";\n")  -- use its previous name
 		else
 			saved[value] = name   -- save name for next time
-			file:write("{}\n")     -- create a new table
+			file:write("{};\n")     -- create a new table
 			for k,v in pairs(value) do      -- save its fields
 				local fieldname = string.format("%s[%s]", name,
 								basicSerialize(k))
 				save(file, fieldname, v, saved)
 			end
 		end
+	elseif type(value) == "boolean" then
+		file:write(basicSerialize(value), ";\n")
 	else
 		error("cannot save a " .. type(value))
 	end
