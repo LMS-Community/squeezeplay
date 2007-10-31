83356d26cf724c12f481c2d486c450c855148cf7
Fred Thomas <slimdevices@thomascorner.com>
2007-10-31 00:48:15 +0000

    Description: Remove _assert patch as defined. New one coming up.
    git-svn-id: https://svn.slimdevices.com/repos/jive/trunk/jive@769 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/pkg/lua-5.1.1/src/lbaselib.c
+++ src/pkg/lua-5.1.1/src/lbaselib.c
@@ -331,13 +331,6 @@ static int luaB_dofile (lua_State *L) {
 }
 
 
-#ifdef LUA_DISABLE_ASSERT
-static int luaB_assertdisabled (lua_State *L) {
-  return 0;
-}
-#endif
-
-
 static int luaB_assert (lua_State *L) {
   luaL_checkany(L, 1);
   if (!lua_toboolean(L, 1))
@@ -450,11 +443,7 @@ static int luaB_newproxy (lua_State *L) {
 
 
 static const luaL_Reg base_funcs[] = {
-#ifdef LUA_DISABLE_ASSERT
-  {"_assert", luaB_assertdisabled},
-#else
   {"_assert", luaB_assert},
-#endif
   {"assert", luaB_assert},
   {"collectgarbage", luaB_collectgarbage},
   {"dofile", luaB_dofile},
