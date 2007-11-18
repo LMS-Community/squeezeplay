d27d06657dccbca8a9ddfd5433cd90419c2b58a8
Adrian Smith <triode1@btinternet.com>
2007-11-18 12:29:48 +0000

    Description: add ability to download wallpapers from the server
    Bug: 5791
    
    git-svn-id: https://svn.slimdevices.com/repos/jive/trunk/jive@927 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/pkg/jive/share/applets/SetupWallpaper/SetupWallpaperApplet.lua
+++ src/pkg/jive/share/applets/SetupWallpaper/SetupWallpaperApplet.lua
@@ -2,12 +2,14 @@
 --[[
 =head1 NAME
 
-applets.SetupWallpaper.SetupWallpaperApplet - The default Jive skin.
+applets.SetupWallpaper.SetupWallpaperApplet - Wallpaper selection.
 
 =head1 DESCRIPTION
 
-This applet implements the default Jive skin. It can
-be used as a model to provide alternate skins for Jive.
+This applet implements selection of the wallpaper for the jive background image.
+
+The applet includes a selection of local wallpapers which are shipped with jive. It
+also allows wallpapers to be downloaded and selected from the currently  attached server.
 
 =head1 FUNCTIONS
 
@@ -19,11 +21,13 @@ SetupWallpaperApplet overrides the following methods:
 
 
 -- stuff we use
-local ipairs = ipairs
+local ipairs, pairs, type, print, tostring = ipairs, pairs, type, print, tostring
 
 local oo                     = require("loop.simple")
-
+local io                     = require("io")
+local os                     = require("os")
 local table                  = require("jive.utils.table")
+local lfs                    = require("lfs")
 
 local Applet                 = require("jive.Applet")
 local AppletManager          = require("jive.AppletManager")
@@ -34,23 +38,26 @@ local SimpleMenu             = require("jive.ui.SimpleMenu")
 local Textarea               = require("jive.ui.Textarea")
 local Tile                   = require("jive.ui.Tile")
 local Window                 = require("jive.ui.Window")
+local Framework              = require("jive.ui.Framework")
+
+local RequestHttp            = require("jive.net.RequestHttp")
+local SocketHttp             = require("jive.net.SocketHttp")
 
 local log                    = require("jive.utils.log").logger("applets.setup")
+local debug                  = require("jive.utils.debug")
 
 local EVENT_FOCUS_GAINED     = jive.ui.EVENT_FOCUS_GAINED
 local EVENT_FOCUS_LOST       = jive.ui.EVENT_FOCUS_LOST
 local EVENT_WINDOW_POP       = jive.ui.EVENT_WINDOW_POP
 
-
-local print, tostring = print, tostring
-
+local jnt                    = jnt
 
 module(...)
 oo.class(_M, Applet)
 
 
 -- Wallpapers
-local wallpapers = {
+local localwallpapers = {
 	{
 		["SUNRISE"] = "sunrise.png",
 	},
@@ -78,43 +85,71 @@ local wallpapers = {
 
 local authors = { "Chapple", "Scott Robinson", "Los Cardinalos", "Orin Optiglot" }
 
+local PREFIX = "applets/SetupWallpaper/wallpaper/"
+
+local REFRESH_TIME = 300 -- only fetch remote wallpapers while browsing if the file is older than this (seconds)
 
 
-function setupShow(self, setupNext)
+function settingsShow(self)
 	local window = Window("window", self:string('WALLPAPER'), 'settingstitle')
-	local menu = SimpleMenu("menu")
-	window:addWidget(menu)
+	self.menu = SimpleMenu("menu")
+	window:addWidget(self.menu)
 
-	local currentPlayer = _getCurrentPlayer()
+	self.currentPlayer = _getCurrentPlayer()
+
 	local wallpaper = self:getSettings()[currentPlayer]
 
-        menu:setComparator(menu.itemComparatorAlphaWeight)
+	self.server = self:_getCurrentServer()
+	self.group  = RadioGroup()
 
-	for weight, section in ipairs(wallpapers) do
+	self.menu:setComparator(SimpleMenu.itemComparatorWeightAlpha)
+	
+	for w, section in ipairs(localwallpapers) do
 		for name, file in table.pairsByKeys(section) do
-			menu:addItem({
-					     text = self:string(name), 
-					     sound = "WINDOWSHOW",
-					     callback = function()
-								self:_setBackground(file, currentPlayer)
-								setupNext()
-							end,
-					     focusGained = function(event)
-								   self:_showBackground(file)
-							   end
-				     })
+			self.menu:addItem({
+				weight = w,
+				text = self:string(name), 
+				sound = "WINDOWSHOW",
+				icon = RadioButton("radio", 
+								   self.group, 
+								   function()
+									   self:_setBackground(file, self.currentPlayer)
+								   end,
+								   wallpaper == file
+							   ),
+				focusGained = function(event)
+								  self:_showBackground(file, self.currentPlayer)
+							  end
+			})
 
-			if wallpaper == file then
-				menu:setSelectedIndex(menu:numItems())
+			if self.wallpaper == file then
+				self.menu:setSelectedIndex(self.menu:numItems())
 			end
 		end
 	end
-	menu:addItem(self:_licenseMenuItem(currentPlayer))
+
+	self.menu:addItem(self:_licenseMenuItem())
+
+	-- look for any server based wallpapers
+	if self.server then
+		log:info("found server - requesting wallpapers list")
+		self.server.comet:request(
+			function(chunk, err)
+				if err then
+					log:debug(err)
+				elseif chunk then
+					self:_serverSink(chunk.data)
+				end
+			end,
+			false,
+			{ "jivewallpapers" }
+		)
+	end
 
 	-- Store the applet settings when the window is closed
 	window:addListener(EVENT_WINDOW_POP,
 		function()
-			self:_showBackground(nil, currentPlayer)
+			self:_showBackground(nil, self.currentPlayer)
 			self:storeSettings()
 		end
 	)
@@ -122,6 +157,7 @@ function setupShow(self, setupNext)
 	self:tieAndShowWindow(window)
 	return window
 end
+
 
 function _getCurrentPlayer(self)
 	local manager = AppletManager:getAppletInstance("SlimDiscovery")
@@ -136,57 +172,59 @@ function _getCurrentPlayer(self)
 	return currentPlayer
 end
 
-function settingsShow(self)
-	local window = Window("window", self:string('WALLPAPER'), 'settingstitle')
-	local menu = SimpleMenu("menu")
-	window:addWidget(menu)
 
-	local currentPlayer = _getCurrentPlayer()
-	local wallpaper = self:getSettings()[currentPlayer]
-	
-	local group = RadioGroup()
-
-        menu:setComparator(menu.itemComparatorAlphaWeight)
-	
-	for weight, section in ipairs(wallpapers) do
-		for name, file in table.pairsByKeys(section) do
-			menu:addItem({
-					     text = self:string(name), 
-					     icon = RadioButton("radio", 
-								group, 
-								function()
-									self:_setBackground(file, currentPlayer)
-								end,
-								wallpaper == file
-							),
-					     focusGained = function(event)
-								   self:_showBackground(file, currentPlayer)
-							   end
-				     })
-
-			if wallpaper == file then
-				menu:setSelectedIndex(menu:numItems())
+function _getCurrentServer(self)
+	local manager = AppletManager:getAppletInstance("SlimDiscovery")
+	local server
+	if manager then
+		if manager:getCurrentPlayer() then
+			server = manager:getCurrentPlayer():getSlimServer()
+		else
+			for _, s in manager:allServers() do
+				server = s
+				break
 			end
 		end
 	end
-
-	menu:addItem(self:_licenseMenuItem(currentPlayer))
-
-	-- Store the applet settings when the window is closed
-	window:addListener(EVENT_WINDOW_POP,
-		function()
-			self:_showBackground(nil, currentPlayer)
-			self:storeSettings()
-		end
-	)
-
-	self:tieAndShowWindow(window)
-	return window
+	return server
 end
 
 
-function _licenseMenuItem(self, playerId)
+function _serverSink(self, data)
+	for _,entry in pairs(data.item_loop) do
+		log:info("server wallpaper: ", entry.name)
+		self.menu:addItem( {
+			weight = 50,	  
+			text = entry.name,
+			icon = RadioButton("radio",
+							   self.group,
+							   function()
+								   local path = Framework:findFile(PREFIX) .. entry.file
+								   local attr = lfs.attributes(path)
+								   if attr then
+									   self:_setBackground(entry.file, self.currentPlayer)
+								   end
+							   end
+						   ),
+			focusGained = function()
+							  local path = Framework:findFile(PREFIX) .. entry.file
+							  local attr = lfs.attributes(path)
+							  if attr and os.time() - attr.modification < REFRESH_TIME then
+								  log:info("using local copy of: ", entry.file)
+								  self:_showBackground(entry.file, self.currentPlayer)
+							  else
+								  log:info("fetching: ", entry.file)
+								  self:_fetchFile(entry.url, path, function() self:_showBackground(entry.file, self.currentPlayer) end)
+							  end
+						  end
+		} )
+	end
+end
+
+
+function _licenseMenuItem(self)
 	return {
+		weight = 99,
 		text = self:string("CREDITS"),
 		sound = "WINDOWSHOW",
 		callback = function()
@@ -202,8 +240,51 @@ function _licenseMenuItem(self, playerId)
 			window:addWidget(Textarea("textarea", text))
 			self:tieAndShowWindow(window)
 		end,
-		focusGained = function(event) self:_showBackground(nil, playerId) end
+		focusGained = function(event) self:_showBackground(nil, self.currentPlayer) end
 	}
+end
+
+
+function _fetchFile(self, url, path, callback)
+	self.last = path
+
+	if self.fetch == nil then
+		self.fetch = {}
+	end
+	if self.fetch[path] then
+		log:warn("already fetching ", path, " not fetching again")
+		return
+	end
+	self.fetch[path] = 1
+
+	local req = RequestHttp(
+		function(chunk, err)
+			self.fetch[path] = nil
+			if err then
+				log:error("error fetching background from server: ", path, " ", url)
+			end
+			if chunk then
+				log:info("fetched background from server: ", path, " ", url)
+				local fh = io.open(path, "wb")
+				if fh then
+					fh:write(chunk)
+					fh:close()
+					if path == self.last then
+						callback()
+					end
+				else
+					log:error("unable to open ", path, " for writing")
+				end
+			end
+		end,
+		'GET',
+		url
+	)
+
+	local uri  = req:getURI()
+	local http = SocketHttp(jnt, uri.host, uri.port, uri.host)
+
+	http:fetch(req)
 end
 
 
@@ -222,7 +303,7 @@ function _showBackground(self, wallpaper, playerId)
 	end
 	self.currentWallpaper = wallpaper
 
-	local srf = Tile:loadImage("applets/SetupWallpaper/wallpaper/" .. wallpaper)
+	local srf = Tile:loadImage(PREFIX .. wallpaper)
 	if srf ~= nil then
 		Framework:setBackground(srf)
 	end
