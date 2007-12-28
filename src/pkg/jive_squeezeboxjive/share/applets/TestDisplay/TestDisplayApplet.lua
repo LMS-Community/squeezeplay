58034df3e13dbc444539f85d52e0747231911688
David Stein <dstein@slimdevices.com>
2007-12-28 15:51:16 +0000

    git-svn-id: https://svn.slimdevices.com/repos/jive/trunk/jive@1271 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/pkg/jive_squeezeboxjive/share/applets/TestDisplay/TestDisplayApplet.lua
+++ src/pkg/jive_squeezeboxjive/share/applets/TestDisplay/TestDisplayApplet.lua
@@ -12,6 +12,8 @@ local Framework           = require("jive.ui.Framework")
 local Popup               = require("jive.ui.Popup")
 local Textarea            = require("jive.ui.Textarea")
 local Font                = require("jive.ui.Font")
+local Timer               = require("jive.ui.Timer")
+local math                = require("math")
 
 local EVENT_KEY_PRESS     = jive.ui.EVENT_KEY_PRESS
 local EVENT_WINDOW_RESIZE = jive.ui.EVENT_WINDOW_RESIZE
@@ -37,12 +39,10 @@ function popupWindow(self, text)
 end
 
 function drawDisplay(self)
-	local y
 	local w, h = Framework:getScreenSize()
 	local srf  = Surface:newRGBA(w, h)
 
 	srf:filledRectangle(0, 0, w, h, 0xFFFFFFFF)
-
 
 	if self.state == 1 then
 		log:info("DisplayTest: White Frame")
@@ -57,94 +57,25 @@ function drawDisplay(self)
 		srf:filledRectangle(0, 0, w, h, 0xFF0000FF)
 
 	elseif self.state == 3 then
-		log:info("DisplayTest: Gradiant RED")
-
-		for y = 0, 160, 1 do
-			local shade = (y * 0xFF / 160)
-			shade = shade - (shade%1)
-			local color = (shade << 24) | 0xFF
-			srf:line( 0, y, 240, y, color)
-		end
-		for y = 0, 160, 1 do
-			local shade = (y * 0xFF / 160)
-			shade = shade - (shade%1)
-			local color = 0xFF000000 | (shade << 16) | (shade << 8) | 0xFF
-			srf:line( 0, y+160, 240, y+160, color)
-		end
-
-	elseif self.state == 4 then
 		log:info("DisplayTest: GREEN")
 		srf:filledRectangle(0, 0, w, h, 0x00FF00FF)
 
-	elseif self.state == 5 then
-		log:info("DisplayTest: Gradiant GREEN")
-
-		for y = 0, 160, 1 do
-			local shade = (y * 0xFF / 160)
-			shade = shade - (shade%1)
-			local color = (shade << 16) | 0xFF
-			srf:line( 0, y, 240, y, color)
-		end
-		for y = 0, 160, 1 do
-			local shade = (y * 0xFF / 160)
-			shade = shade - (shade%1)
-			local color = (shade << 24) | 0xFF0000 | (shade << 8) | 0xFF
-			srf:line( 0, y+160, 240, y+160, color)
-		end
-	
-	elseif self.state == 6 then
+	elseif self.state == 4 then
 		log:info("DisplayTest: BLUE")
 		srf:filledRectangle(0, 0, w, h, 0x0000FFFF)
-	
-	elseif self.state == 7 then
-		log:info("DisplayTest: Gradiant BLUE")
-		srf:filledRectangle(0, 0, w, h, 0x0000FFFF)
 
-		for y = 0, 160, 1 do
-			local shade = (y * 0xFF / 160)
-			shade = shade - (shade%1)
-			local color = (shade << 8) | 0xFF
-			srf:line( 0, y, 240, y, color)
-		end
-		for y = 0, 160, 1 do
-			local shade = (y * 0xFF / 160)
-			shade = shade - (shade%1)
-			local color = (shade << 24) | (shade << 16) | 0xFFFF
-			srf:line( 0, y+160, 240, y+160, color)
-		end
-
-	elseif self.state == 8 then
+	elseif self.state == 5 then
 		log:info("DisplayTest: WHITE")
 		srf:filledRectangle(0, 0, w, h, 0xFFFFFFFF)
 
-	elseif self.state == 9 then
-		log:info("DisplayTest: Gradiant WHITE")
-
-		for y = 0, 320, 1 do
-			local shade = (y * 0xFF / 320)
-			shade = shade - (shade%1)
-			local color = (shade << 24) | (shade << 16) | (shade << 8) | 0xFF
-			srf:line( 0, y, 240, y, color)
-		end
-
-	elseif self.state == 10 then
+	elseif self.state == 6 then
 		log:info("DisplayTest: BLACK")
 		srf:filledRectangle(0, 0, w, h, 0x000000FF)
 
-	elseif self.state == 11 then
-		log:info("DisplayTest: HORZ_STRIPES")
-		srf:filledRectangle(0, 0, w, h, 0xFFFFFFFF)
-		for y = 0, h, 2 do
-			srf:line(0, y, w, y, 0x000000FF)
-		end
-
-	elseif self.state == 12 then
-		log:info("DisplayTest: VERT_STRIPES")
-		srf:filledRectangle(0, 0, w, h, 0xFFFFFFFF)
-		for x = 0, w, 2 do
-			srf:line(x ,0, x, h, 0x000000FF)
-		end
-
+	elseif self.state == 7 then
+		log:info("DisplayTest: GRADIENT")
+		srf:filledRectangle(0, 0, w, h, 0x000000FF)
+		srf = Surface:loadImage("applets/TestDisplay/circle.jpg")
 	else
 		log:info("DisplayTest: TEST_COMPLETE")
 		self:popupWindow(self:string("TEST_COMPLETE"))
