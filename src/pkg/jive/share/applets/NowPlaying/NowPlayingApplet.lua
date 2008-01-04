b5183f8b9669b133adbba09dca2a113e456a0dff
Ben Klaas <bklaas@logitech.com>
2008-01-04 05:23:16 +0000

    Layout changes to support different types of items: 	Radio icon for streams with no artwork 	Removal of progress bar and assoc layout changes for streaming content with no track duration info 	Static content for service providers (e.g., RadioIO) that do not provide artwork
    Removal of drop shadows per 3 Jan Eng campfire discussion
    Tweaking of layout to provide larger art (this will need further tweaking after Noah provides new reference screens)
    
    Bug: 6265, 6328, 5404
    
    git-svn-id: https://svn.slimdevices.com/repos/jive/trunk/jive@1310 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/pkg/jive/share/applets/NowPlaying/NowPlayingApplet.lua
+++ src/pkg/jive/share/applets/NowPlaying/NowPlayingApplet.lua
@@ -71,10 +71,11 @@ module(...)
 oo.class(_M, Applet)
 
 -- with drop shadow
-local ARTWORK_SIZE = 154
+--local ARTWORK_SIZE = 154
 -- without drop shadow
---local ARTWORK_SIZE = 166
+local ARTWORK_SIZE = 166
 
+local showProgressBar = true
 
 ----------------------------------------------------------------------------------------
 -- Helper Functions
@@ -103,6 +104,18 @@ local function _nowPlayingArtworkThumbUri(iconId)
         return '/music/' .. iconId .. '/cover_' .. ARTWORK_SIZE .. 'x' .. ARTWORK_SIZE .. '_p.png'
 end
 
+local function _staticArtworkThumbUri(path)
+	local resizedImg = path
+	-- 'p' is for padded, png gives us transparency
+	local resizeFrag = '_' .. ARTWORK_SIZE .. 'x' .. ARTWORK_SIZE .. '_p.png' 
+	local artworkUri = path
+	-- replace .png with the resize params
+	if string.match(path, '.png') then
+                artworkUri = string.gsub(path, '.png', resizeFrag)
+	end
+	return artworkUri
+end
+
 local function SecondsToString(seconds)
 	local min = math.floor(seconds / 60)
 	local sec = math.floor(seconds - (min*60))
@@ -110,19 +123,19 @@ local function SecondsToString(seconds)
 	return string.format("%d:%02d", min, sec)
 end
 
-local function _getIcon(self, item, icon)
+local function _getIcon(self, item, icon, remote)
 	local server = self.player:getSlimServer()
 
 	if windowStyle == 'ss' then
 		-- without drop shadow
-		--ARTWORK_SIZE = 184
+		ARTWORK_SIZE = 190
 		-- with drop shadow
-		ARTWORK_SIZE = 172
+		--ARTWORK_SIZE = 172
 	else
 		-- without drop shadow
-		--ARTWORK_SIZE = 166
+		ARTWORK_SIZE = 166
 		-- with drop shadow
-		ARTWORK_SIZE = 154
+		--ARTWORK_SIZE = 154
 	end
 
 	if item and item["icon-id"] then
@@ -130,7 +143,16 @@ local function _getIcon(self, item, icon)
 		server:fetchArtworkThumb(item["icon-id"], icon, _nowPlayingArtworkThumbUri, ARTWORK_SIZE) 
 	elseif item and item["icon"] then
 		-- Fetch a remote image URL, sized to ARTWORK_SIZE x ARTWORK_SIZE
-		server:fetchArtworkURL(item["icon"], icon, ARTWORK_SIZE)
+		local remoteContent = string.find(item['icon'], 'http://')
+		-- sometimes this is static content
+		if remoteContent then
+			server:fetchArtworkURL(item["icon"], icon, ARTWORK_SIZE)
+		else
+			server:fetchArtworkThumb(item["icon"], icon, _staticArtworkThumbUri, ARTWORK_SIZE) 
+		end
+	elseif item and item["params"] and item["params"]["track_id"] then
+		-- this is for the radio image-- remote URLs with no icon (Bug 6087)
+		server:fetchArtworkThumb(item["params"]["track_id"], icon, _nowPlayingArtworkThumbUri, ARTWORK_SIZE) 
 	else
 		icon:setValue(nil)
 	end
@@ -151,8 +173,10 @@ local function updatePosition(self)
 	end
 
 	self.progressGroup:setWidgetValue("elapsed", strElapsed)
-	self.progressGroup:setWidgetValue("remain", strRemain)
-	self.progressSlider:setValue(pos)
+	if showProgressBar then
+		self.progressGroup:setWidgetValue("remain", strRemain)
+		self.progressSlider:setValue(pos)
+	end
 end
 
 function init(self)
@@ -186,11 +210,13 @@ function updateProgress(self, trackpos, tracklen)
 	self.trackPos = trackpos
 	self.trackLen = tracklen	
 
-	if tracklen and tracklen > 0 then
-		self.progressSlider:setRange(0, tracklen, trackpos)
-	else 
-		-- If 0 just set it to 100
-		self.progressSlider:setRange(0, 100, 0)
+	if self.progressSlider then
+		if tracklen and tracklen > 0 then
+			self.progressSlider:setRange(0, tracklen, trackpos)
+		else 
+			-- If 0 just set it to 100
+			self.progressSlider:setRange(0, 100, 0)
+		end
 	end
 
 	updatePosition(self)
@@ -244,7 +270,7 @@ function _process_status(self, data)
 
 		-- create new window
 		-- FIXME this can now be combined in _process_status, may help fixing bug 6087
-		_createUI(self)
+		_createUI(self, data)
 		createdWindow = true
 
 		self.track_id = data.item_loop[1].params.track_id
@@ -262,24 +288,28 @@ function _process_status(self, data)
 
 	if data.item_loop then
 		local text = data.item_loop[1].text
+		local showProgressBar = true
 		-- XXX: current_title of null is a function value??
 		if data.remote == 1 and type(data.current_title) == 'string' then 
 			text = text .. "\n" .. data.current_title
 		end
+		if data.time == 0 then
+			showProgressBar = false
+		end
 
 		local item = data.item_loop[1]
 
-		_getIcon(self, item, self.artwork)
+		_getIcon(self, item, self.artwork, data.remote)
 		self:updateTrack(text)
 		self:updateProgress(data.time, data.duration)
 		self:updatePlaylist(true, data.playlist_cur_index, data.playlist_tracks)
 
 		-- preload artwork for next track
 		if data.item_loop[2] then
-			_getIcon(self, data.item_loop[2], Icon("artwork"))
+			_getIcon(self, data.item_loop[2], Icon("artwork"), data.remote)
 		end
 	else
-		_getIcon(self, nil, self.artwork)
+		_getIcon(self, nil, self.artwork, nil)
 		self:updateTrack("\n\n\n")
 		self:updateProgress(0, 0)
 		self:updatePlaylist(false, 0, 0)
@@ -289,7 +319,7 @@ function _process_status(self, data)
 end
 
 
-function _createUI(self)
+function _createUI(self, data)
 	local window = Window("window")
 
 	if windowStyle == 'ss' then
@@ -300,11 +330,18 @@ function _createUI(self)
 		window:addWidget(Icon("iconbg", srf))
 	end
 
+	if data.duration then
+		showProgressBar = true
+	else
+		showProgressBar = false
+	end
+
 	local components = { 
 		nptitle = "nptitle", 
 		nptrack = "nptrack", 
 		progressB = "progressB", 
 		progress = "progress", 
+		progressNB = "progressNB",
 		npartwork = "npartwork" 
 	}
 	for k, v in pairs(components) do
@@ -323,14 +360,21 @@ function _createUI(self)
 			   })
 	
 	
-	self.progressSlider = Slider(components.progressB, 0, 100, 0)
-	self.progressSlider:addTimer(1000, function() self:tick() end)
+	if showProgressBar then
+		self.progressSlider = Slider(components.progressB, 0, 100, 0)
+		self.progressSlider:addTimer(1000, function() self:tick() end)
 
-	self.progressGroup = Group(components.progress, {
-				      elapsed = Label("text", ""),
-				      slider = self.progressSlider,
-				      remain = Label("text", "")
-			      })
+		self.progressGroup = Group(components.progress, {
+					      elapsed = Label("text", ""),
+					      slider = self.progressSlider,
+					      remain = Label("text", "")
+				      })
+	else
+		self.progressGroup = Group(components.progressNB, {
+					      elapsed = Label("text", "")
+				      })
+		self.progressGroup:addTimer(1000, function() self:tick() end)
+	end
 
 	self.artwork = Icon("artwork")
 	artworkGroup = Group(components.npartwork, {
@@ -514,7 +558,7 @@ function skin(self, s)
         s.ssnptrack.bgImg = highlightBox
 	s.ssnptrack.text = {}
 	s.ssnptrack.text.w = WH_FILL
-	s.ssnptrack.text.padding = { 10, 7, 8, 9 }
+	s.ssnptrack.text.padding = { 8, 7, 8, 2 }
 	s.ssnptrack.text.align = "top-left"
         s.ssnptrack.text.font = Font:load(fontpath .. "FreeSans.ttf", 14)
 	s.ssnptrack.text.lineHeight = 17
@@ -531,16 +575,16 @@ function skin(self, s)
 
 	-- Artwork
 	local browseArtWidth = 166
-	local ssArtWidth = 184
+	local ssArtWidth = 190
 
 	local ssnoartworkoffset = (screenWidth - ssArtWidth) / 2
 	s.ssnpartwork = {}
 	s.ssnpartwork.w = ssArtWidth
 	s.ssnpartwork.border = { ssnoartworkoffset, 4, ssnoartworkoffset, 6 }
-	s.ssnpartwork.align = "center"
-	s.ssnpartwork.bgImg = Tile:loadImage(imgpath .. "album_shadow_" .. ssArtWidth .. ".png")
+	s.ssnpartwork.align = "bottom-right"
+--	s.ssnpartwork.bgImg = Tile:loadImage(imgpath .. "album_shadow_" .. ssArtWidth .. ".png")
 	s.ssnpartwork.artwork = {}
-	s.ssnpartwork.artwork.padding = 3
+	s.ssnpartwork.artwork.padding = 1 
 	s.ssnpartwork.artwork.img = Surface:loadImage(imgpath .. "album_noartwork_" .. ssArtWidth .. ".png")
 
 	-- artwork layout is not the same between the two windowStyles
@@ -548,8 +592,8 @@ function skin(self, s)
 	local browsenpartwork = {
 		w = browseArtWidth,
 		border = { browsenoartworkoffset, 4, browsenoartworkoffset, 6 },
-		bgImg = Tile:loadImage(imgpath .. "album_shadow_" .. browseArtWidth .. ".png"),
-		artwork = { padding = 3, img = Surface:loadImage(imgpath .. "album_noartwork_" .. browseArtWidth .. ".png") }
+		--bgImg = Tile:loadImage(imgpath .. "album_shadow_" .. browseArtWidth .. ".png"),
+		artwork = { padding = 1, img = Surface:loadImage(imgpath .. "album_noartwork_" .. browseArtWidth .. ".png") }
 	}
 	s.browsenpartwork = _uses(s.ssnpartwork, browsenpartwork)
 
@@ -572,7 +616,7 @@ function skin(self, s)
 	s.ssprogress.order = { "elapsed", "slider", "remain" }
 	s.ssprogress.text = {}
 	s.ssprogress.text.w = 50
-	s.ssprogress.padding = { 0, 10, 0, 0 }
+	s.ssprogress.padding = { 0, 0, 0, 0 }
 	s.ssprogress.text.padding = { 8, 0, 8, 0 }
 	s.ssprogress.text.font = Font:load(fontpath .. "FreeSansBold.ttf", 12)
 	s.ssprogress.text.fg = { 0xe7,0xe7, 0xe7 }
@@ -591,7 +635,21 @@ function skin(self, s)
 
 	s.browseprogressB = _uses(s.ssprogressB)
 
-	-- background style should start at x,y = 0,0
+	-- special style for when there shouldn't be a progress bar (e.g., internet radio streams)
+	s.ssprogressNB = {}
+	s.ssprogressNB.order = { "elapsed" }
+	s.ssprogressNB.text = {}
+	s.ssprogressNB.text.w = WH_FILL
+	s.ssprogressNB.text.align = "center"
+	s.ssprogressNB.padding = { 0, 0, 0, 0 }
+	s.ssprogressNB.text.padding = { 0, 0, 0, 0 }
+	s.ssprogressNB.text.font = Font:load(fontpath .. "FreeSansBold.ttf", 12)
+	s.ssprogressNB.text.fg = { 0xe7, 0xe7, 0xe7 }
+	s.ssprogressNB.text.sh = { 0x37, 0x37, 0x37 }
+
+	s.browseprogressNB = _uses(s.ssprogressNB)
+
+-- background style should start at x,y = 0,0
         s.iconbg = {}
         s.iconbg.x = 0
         s.iconbg.y = 0
