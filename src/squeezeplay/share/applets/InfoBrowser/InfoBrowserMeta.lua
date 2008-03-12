4dd6e727d0a6cf561be8e2b1f3bba512a28e81b2
Adrian Smith <triode1@btinternet.com>
2008-03-12 17:23:27 +0000

    Description: remove infobrowser from menus when no player attached
    Bug: 6949
    
    git-svn-id: https://svn.slimdevices.com/repos/jive/trunk/squeezeplay@2104 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/share/applets/InfoBrowser/InfoBrowserMeta.lua
+++ src/squeezeplay/share/applets/InfoBrowser/InfoBrowserMeta.lua
@@ -13,6 +13,7 @@ local AppletMeta    = require("jive.AppletMeta")
 local appletManager = appletManager
 local jiveMain      = jiveMain
 
+local jnt           = jnt
 
 module(...)
 oo.class(_M, AppletMeta)
@@ -24,7 +25,17 @@ end
 
 
 function registerApplet(meta)
-	jiveMain:addItem(
-		meta:menuItem('appletInfoBrowser', 'extras', meta:string('INFOBROWSER'), function(applet, ...) applet:menu(...) end, 10)
-	)
+	jnt:subscribe(meta)
+	meta.menu = meta:menuItem('appletInfoBrowser', 'extras', meta:string('INFOBROWSER'), function(applet, ...) applet:menu(...) end, 10)
+end
+
+
+-- only show on the menu if a player is attached
+-- this allows SN to use the player id to select which information feeds to show
+function notify_playerCurrent(meta, player)
+	if player == nil then
+		jiveMain:removeItem(meta.menu)
+	else
+		jiveMain:addItem(meta.menu)
+	end
 end
