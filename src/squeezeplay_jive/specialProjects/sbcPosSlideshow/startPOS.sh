45982b0d7b01e624d96207ddfad0cecce2dcaeac
Ben Klaas <bklaas@logitech.com>
2008-03-12 16:18:34 +0000

    Description: putting slideshow demo written for POS units in Europe under revision control
    git-svn-id: https://svn.slimdevices.com/repos/jive/trunk/squeezeplay@2103 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

---
+++ src/squeezeplay_jive/specialProjects/sbcPosSlideshow/startPOS.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+rm -rf /usr/share/jive/applets/Slideshow
+cp -rf /mnt/mmc/Slideshow /usr/share/jive/applets/.
+cp -rf /mnt/mmc/popImages /usr/share/jive/applets/Slideshow/images
+cp -f /mnt/mmc/screenSettings.lua /usr/share/jive/applets/SqueezeboxJive/settings.lua
+cp -f /mnt/mmc/setupWelcomeSettings.lua /usr/share/jive/applets/SetupWelcome/settings.lua
+cp -f /mnt/mmc/screensaverSettings.lua /usr/share/jive/applets/ScreenSavers/settings.lua
+cp -f /mnt/mmc/SqueezeboxJiveApplet.lua /usr/share/jive/applets/SqueezeboxJive/SqueezeboxJiveApplet.lua
+/usr/bin/jive &
+
