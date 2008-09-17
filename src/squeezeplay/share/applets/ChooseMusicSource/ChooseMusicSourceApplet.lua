LMS-Community/squeezeplay

commit c1e06a2aad8be8909b8aa76627d33c4103485782

Description: allow other music sources to be selected from boom
Bug: 9307

git-svn-id: https://svn.slimdevices.com/repos/jive/7.1/trunk/squeezeplay@2907 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

src/squeezeplay/share/applets/ChooseMusicSource/ChooseMusicSourceApplet.lua
@@ -126,8 +126,7 @@ end
 function _addServerItem(self, server, address)
 	log:debug("_addServerItem ", server, " " , port)
 
-	local id = server or address
-
+	local id = server:getIpPort() or address
 
 	-- remove existing entry
 	if self.serverList[id] then
@@ -165,8 +164,6 @@ function _addServerItem(self, server, address)
 	if currentPlayer and currentPlayer:getSlimServer() and server == currentPlayer:getSlimServer() then
 		item.style = 'checkedNoAction'
 		item.callback = nil
-	elseif not currentPlayer or not currentPlayer:canConnectToServer() then
-		return
 	end
 
 	self.serverMenu:addItem(item)

src/squeezeplay/share/jive/slim/Player.lua
@@ -81,7 +81,7 @@ local DEVICE_IDS = {
 	[4] = "squeezebox2",
 	[5] = "transporter",
 	[7] = "receiver",
-	[10] = "b",
+	[10] = "boom",
 }
 
 
@@ -655,7 +655,7 @@ function getMacAddress(self)
 	if self.info.model == "squeezebox2"
 		or self.info.model == "receiver"
 		or self.info.model == "transporter" 
-		or self.info.model == "b" then
+		or self.info.model == "boom" then
 
 		return string.gsub(self.id, "[^%x]", "")
 	end
@@ -1156,7 +1156,7 @@ function canUdap(self)
 	return self.info.model == "squeezebox2"
 		or self.info.model == "receiver"
 		or self.info.model == "transporter"
-		or self.info.model == "b"
+		or self.info.model == "boom"
 end
 
 
@@ -1165,7 +1165,7 @@ function canConnectToServer(self)
 	return self.info.model == "squeezebox2"
 		or self.info.model == "receiver"
 		or self.info.model == "transporter"
-		or self.info.model == "b"
+		or self.info.model == "boom"
 end
 
 

commit f9582d3de6338046f13541c2f51ff39c450d3e84

Description: fix problem caused by r2907 where server:getIpPort() was being called in situations where server was nil add a bunch of log:debug() flags for future use
Bug: 9307

git-svn-id: https://svn.slimdevices.com/repos/jive/7.1/trunk/squeezeplay@2920 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

src/squeezeplay/share/applets/ChooseMusicSource/ChooseMusicSourceApplet.lua
@@ -77,22 +77,27 @@ function settingsShow(self)
 	jnt:subscribe(self)
 
 
-	-- Discover slimservers in this window
+	-- Discover players in this window
 	appletManager:callService("discoverPlayers")
 	window:addTimer(1000, function() appletManager:callService("discoverPlayers") end)
 
 
-	-- slimservers on the poll list
+	-- squeezecenter on the poll list
+	log:debug("*****Polled Server List:")
 	local poll = appletManager:callService("getPollList")
 	for address,_ in pairs(poll) do
+		log:debug('Found: ', address)
 		if address ~= "255.255.255.255" then
+			log:debug('Add to menu: ', address)
 			self:_addServerItem(nil, address)
 		end
 	end
 
 
-	-- discovered slimservers
+	-- discovered squeezecenters
+	log:debug('*****Discovered Server List:')
 	for _,server in appletManager:callService("iterateSqueezeCenters") do
+		log:debug('discovered server: ', server)
 		self:_addServerItem(server)
 	end
 
@@ -124,9 +129,15 @@ end
 
 
 function _addServerItem(self, server, address)
-	log:debug("_addServerItem ", server, " " , port)
+	log:debug("\t_addServerItem ", server, " " , address)
 
-	local id = server:getIpPort() or address
+	local id
+	if server then
+		id = server:getIpPort()
+	else
+		id = address
+	end
+	log:debug("\tid for this server set to: ", id)
 
 	-- remove existing entry
 	if self.serverList[id] then
@@ -141,6 +152,7 @@ function _addServerItem(self, server, address)
 	-- new entry
 	local item
 	if server and currentPlayer and currentPlayer:canConnectToServer() then
+		log:debug("\tadd menu item with callback")
 		local f = function()
 				  self:connectPlayer(currentPlayer, server)
 			  end
@@ -153,6 +165,7 @@ function _addServerItem(self, server, address)
 			weight = 1
 		}
 	else
+		log:debug("\tadd menu item without callback")
 		item = {
 			text = server and server:getName() or address,
 			weight = 1,
@@ -162,6 +175,7 @@ function _addServerItem(self, server, address)
 
 	-- check current player
 	if currentPlayer and currentPlayer:getSlimServer() and server == currentPlayer:getSlimServer() then
+		log:debug("\tthis is the connected server, so remove callback for this item")
 		item.style = 'checkedNoAction'
 		item.callback = nil
 	end