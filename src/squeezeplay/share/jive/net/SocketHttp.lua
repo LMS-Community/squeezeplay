19c8c3e1806c3b4b06044556796d0d2cb803b1d5
Richard Titmuss <rtitmuss@logitech.com>
2008-10-29 20:57:20 +0000

    Revert the timeout change, we wait for 70 seconds to get the http headers.
    Bug: 8159
    
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.3/trunk/squeezeplay@3232 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/share/jive/net/SocketHttp.lua
+++ src/squeezeplay/share/jive/net/SocketHttp.lua
@@ -63,7 +63,7 @@ local BLOCKSIZE = 4096
 
 -- timeout for socket operations
 local SOCKET_CONNECT_TIMEOUT = 10 -- connect in 10 seconds
-local SOCKET_TIMEOUT = 70 -- response in 70 seconds
+local SOCKET_BODY_TIMEOUT = 70 -- response in 70 seconds
 
 -- http authentication credentials
 local credentials = {}
@@ -520,7 +520,7 @@ function t_rcvHeaders(self)
 		end
 	end
 	
-	self:t_addRead(pump, SOCKET_CONNECT_TIMEOUT)
+	self:t_addRead(pump, SOCKET_BODY_TIMEOUT)
 end
 
 
@@ -814,7 +814,7 @@ function t_rcvResponse(self)
 		end
 	end
 	
-	self:t_addRead(pump, SOCKET_TIMEOUT)
+	self:t_addRead(pump, SOCKET_BODY_TIMEOUT)
 end
 
 
