e6fbab369c06286f250827af6ff7a5378476af25
Ben Klaas <bklaas@logitech.com>
2008-11-14 19:46:30 +0000

    Description: move playerstatus subscription from subscribe:30 to subscribe:0 this means playerstatus updates will stop coming every 30 seconds, but will be generated on an as-needed basis by SC. there is some chance that this will help the NAS disk spin up issue
    Bug: 8300, 9102
    
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.3/trunk/squeezeplay@3376 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/share/jive/slim/Player.lua
+++ src/squeezeplay/share/jive/slim/Player.lua
@@ -764,7 +764,7 @@ function onStage(self)
 	self.slimServer.comet:startBatch()
 	
 	-- subscribe to player status updates
-	local cmd = { 'status', '-', 10, 'menu:menu', 'subscribe:30' }
+	local cmd = { 'status', '-', 10, 'menu:menu', 'subscribe:0' }
 	self.slimServer.comet:subscribe(
 		'/slim/playerstatus/' .. self.id,
 		_getSink(self, cmd),
