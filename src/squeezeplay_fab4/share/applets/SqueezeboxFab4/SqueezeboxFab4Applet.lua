a7631c20f83abe7717d285b33f64f80926957780
Richard Titmuss <rtitmuss@logitech.com>
2008-11-27 21:26:17 +0000

    Update fab4 applet for recent changes. Added nag screen for incorrect uuid/mac address. Added watchdog timer.
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/private-branches/fab4/squeezeplay@3462 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay_fab4/share/applets/SqueezeboxFab4/SqueezeboxFab4Applet.lua
+++ src/squeezeplay_fab4/share/applets/SqueezeboxFab4/SqueezeboxFab4Applet.lua
@@ -1,6 +1,4 @@
 
-local print = print
-
 -- board specific driver
 local fab4_bsp               = require("fab4_bsp")
 
@@ -8,7 +6,16 @@ local oo                     = require("loop.simple")
 local io                     = require("io")
 local string                 = require("string")
 
+local Applet                 = require("jive.Applet")
+local System                 = require("jive.System")
+
 local Framework              = require("jive.ui.Framework")
+local Popup                  = require("jive.ui.Popup")
+local SimpleMenu             = require("jive.ui.SimpleMenu")
+local Textarea               = require("jive.ui.Textarea")
+local Timer                  = require("jive.ui.Timer")
+
+local Watchdog               = require("jiveWatchdog")
 
 local debug                  = require("jive.utils.debug")
 local log                    = require("jive.utils.log").logger("applets.setup")
@@ -21,17 +28,80 @@ oo.class(_M, Applet)
 
 
 function init(self)
-	-- FIXME uuid
-	local uuid = "00000000000000000000000000000000"
+	local uuid, mac
 
-	-- read device mac
-	local f = io.popen("/sbin/ifconfig eth0")
+	-- read device uuid
+	local f = io.open("/proc/cpuinfo")
 	if f then
-	 	local ifconfig = f:read("*all")
+		for line in f:lines() do
+			if string.match(line, "UUID") then
+				uuid = string.match(line, "UUID%s+:%s+([%x-]+)")
+				uuid = string.gsub(uuid, "[^%x]", "")
+			end
+		end
 		f:close()
-
-		mac = string.match(ifconfig, "HWaddr%s(%x%x:%x%x:%x%x:%x%x:%x%x:%x%x)")
 	end
 
-	jnt:setUUID(uuid, mac)
+	System:init({
+		uuid = uuid,
+		machine = "fab4",
+	})
+
+	mac = System:getMacAddress()
+	uuid = System:getUUID()
+
+	if not uuid or string.match(mac, "^00:40:20") then
+		local popup = Popup("errorWindow", self:string("INVALID_MAC_TITLE"))
+
+		popup:setAllowScreensaver(false)
+		popup:setAlwaysOnTop(true)
+		popup:setAutoHide(false)
+
+		local text = Textarea("textarea", self:string("INVALID_MAC_TEXT"))
+		local menu = SimpleMenu("menu", {
+			{
+				text = self:string("INVALID_MAC_CONTINUE"),
+				sound = "WINDOWHIDE",
+				callback = function()
+						   popup:hide()
+					   end
+			},
+		})
+
+		popup:addWidget(text)
+		popup:addWidget(menu)
+		popup:show()
+	end
+
+
+	-- watchdog timer
+	self.watchdog = Watchdog:open()
+	if self.watchdog then
+		-- allow 30 seconds to boot
+		self.watchdog:setTimeout(30)
+		local timer = Timer(2000, -- 2 seconds
+			function()
+				-- 10 second when running
+				if not self.watchdogRunning then
+					self.watchdog:setTimeout(10)
+					self.watchdogRunning = true
+				end
+				self.watchdog:keepAlive()
+			end)
+		timer:start()
+	else
+		log:warn("Watchdog timer is disabled")
+	end
 end
+
+
+--[[
+
+=head1 LICENSE
+
+Copyright 2007 Logitech. All Rights Reserved.
+
+This file is subject to the Logitech Public Source License Version 1.0. Please see the LICENCE file for details.
+
+=cut
+--]]
