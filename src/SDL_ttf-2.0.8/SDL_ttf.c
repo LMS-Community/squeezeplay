c2590e58340604dfaf103e5393a698d077a651d4
Tom Wadzinski <tom_wadzinski@logitech.com>
2008-12-11 21:34:43 +0000

    fixes for new freetype version, similar to patch files being used in poky
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/branches/pango/squeezeplay@3502 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/SDL_ttf-2.0.8/SDL_ttf.c
+++ src/SDL_ttf-2.0.8/SDL_ttf.c
@@ -48,7 +48,7 @@
 #include <freetype/ftoutln.h>
 #include <freetype/ttnameid.h>
 */
-#include <freetype/internal/ftobjs.h>
+//#include <freetype/internal/ftobjs.h>
 
 #ifndef FT_OPEN_STREAM
 #define FT_OPEN_STREAM ft_open_stream
@@ -278,7 +278,7 @@ TTF_Font* TTF_OpenFontIndexRW( SDL_RWops *src, int freesrc, int ptsize, long ind
 	}
 	memset(stream, 0, sizeof(*stream));
 
-	stream->memory = library->memory;
+//	stream->memory = library->memory;
 	stream->read = RWread;
 	stream->descriptor.pointer = src;
 	stream->pos = (unsigned long)position;
