beb7709a387aafb7b5338a947a7c3f9826017471
Ben Klaas <bklaas@logitech.com>
2009-01-30 04:45:59 +0000

    Description: make setup applet launch automatically at boot if setup hasn't been previously completed
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/private-branches/fab4/squeezeplay@3989 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay_fab4/share/applets/SetupWelcome/SetupWelcomeMeta.lua
+++ src/squeezeplay_fab4/share/applets/SetupWelcome/SetupWelcomeMeta.lua
@@ -44,22 +44,12 @@ end
 
 function registerApplet(meta)
 	meta:registerService(startingStep)
-	-- for development, add a menu item for this
-	jiveMain:addItem(meta:menuItem('appletSetupWelcome', 'home', "SETUP_DEVELOPMENT",
-		function(applet, ...)
-			applet:getSettings().setupDone = false
-			applet:step1(...)
-		end
-		, 100))
-
 end
 
 function configureApplet(meta)
---[[ DISABLE UNTIL COMPLETE
 	if not meta:getSettings().setupDone then
 		appletManager:callService(startingStep)
 	end
---]]
 end
 
 --[[
