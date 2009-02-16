e4ce5360ebbdae95b48ddf80681d8056375f511d
Matt Wise <mwise@slimdevices.com>
2009-02-16 19:50:34 +0000

    Basic startup script for SqueezePlay on Linux.
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/trunk/squeezeplay@4227 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay_desktop/squeezeplay.sh
+++ src/squeezeplay_desktop/squeezeplay.sh
@@ -1,1 +1,17 @@
-#placeholder for Matt W.
+#!/bin/sh
+
+##
+## This script is a basic startup script for the SqueezePlay binary (jive) that requires a few environment variables be set.
+##
+
+## Change these if you changed your install path
+INSTALL_DIR=/opt/squeezeplay/
+LIB_DIR=$INSTALL_DIR/lib
+INC_DIR=$INSTALL_DIR/inc
+
+## Start up
+export LD_LIBRARY_PATH=$LIB_DIR:$LD_LIBRARY_PATH
+export LD_INCLUDE_PATH=$INC_DIR:$LD_INCLUDE_PATH
+
+$INSTALL_DIR/bin/jive
+
