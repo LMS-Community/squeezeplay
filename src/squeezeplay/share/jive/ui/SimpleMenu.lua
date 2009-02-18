LMS-Community/squeezeplay

commit 4882943c5503ecc701c3746b6753b3b8efa332a6

Mini applet to assist debugging skins, enable/disable using } on the keyboard, then mouse around.
git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/trunk/squeezeplay@4248 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

src/squeezeplay/share/jive/ui/SimpleMenu.lua
@@ -114,8 +114,9 @@ local function _itemRenderer(menu, list, widgetList, indexList, size)
 			local item = list[indexList[i]]
 
 			local icon = item.icon or menu.icons[i]
+			local iconStyle = item.iconStyle or "icon"
 			if icon == nil then
-				icon = Icon("icon")
+				icon = Icon(iconStyle)
 				menu.icons[i] = icon
 			end
 
@@ -129,7 +130,7 @@ local function _itemRenderer(menu, list, widgetList, indexList, size)
 			else
 				widgetList[i]:setStyle(item.style or "item")
 				widgetList[i]:setWidgetValue("text", item.text)
-				widgetList[i]:setWidget("icon", icon)
+				widgetList[i]:setWidget(iconStyle, icon)
 			end
 		end
 	end

src/squeezeplay/share/applets/DebugSkin/DebugSkinMeta.lua
@@ -1,0 +1,90 @@
+
+local oo            = require("loop.simple")
+
+local AppletMeta    = require("jive.AppletMeta")
+
+local Canvas        = require("jive.ui.Canvas")
+local Framework     = require("jive.ui.Framework")
+
+local debug         = require("jive.utils.debug")
+local log           = require("jive.utils.log").logger("ui")
+
+local appletManager = appletManager
+
+
+module(..., Framework.constants)
+oo.class(_M, AppletMeta)
+
+
+function jiveVersion(meta)
+	return 1, 1
+end
+
+
+function defaultSettings(meta)
+end
+
+
+function registerApplet(meta)
+end
+
+
+function configureApplet(meta)
+	Framework:addActionListener("debug_skin", meta, _debugSkin, 9999)
+end
+
+
+function _debugWidget(meta, screen, widget)
+	if meta.mouseEvent and widget:mouseInside(meta.mouseEvent) then
+		local x,y,w,h = widget:getBounds()
+		screen:filledRectangle(x,y, x+w,y+h, 0xFF00003F)
+
+		log:info("-> ", widget, " (", x, ",", y, " ", w, "x", h, ")")
+	end
+
+	widget:iterate(function(child)
+		_debugWidget(meta, screen, child)
+	end)
+end
+
+
+function _debugSkin(meta)
+	if meta.enabled then
+		meta.enabled = nil
+
+		Framework:removeWidget(meta.canvas)
+		Framework:removeListener(meta.mouseListener)
+
+		return
+	end
+
+	meta.enabled = true
+
+	meta.canvas = Canvas("blank", function(screen)
+		local window = Framework.windowStack[1]
+
+		log:info("Mouse in: ", window)
+		window:iterate(function(w)
+			_debugWidget(meta, screen, w)
+		end)
+	end)
+	Framework:addWidget(meta.canvas)
+
+	meta.mouseListener = Framework:addListener(EVENT_MOUSE_ALL,
+		function(event)
+			meta.mouseEvent = event
+			Framework:reDraw(nil)
+		end, -99)
+end
+
+
+--[[
+
+=head1 LICENSE
+
+Copyright 2007 Logitech. All Rights Reserved.
+
+This file is subject to the Logitech Public Source License Version 1.0. Please see the LICENCE file for details.
+
+=cut
+--]]

src/squeezeplay/share/jive/InputToActionMap.lua
@@ -57,6 +57,7 @@ charActionMappings.press = {
 	["7"]  = "play_favorite_7",
 	["8"]  = "play_favorite_8",
 	["9"]  = "play_favorite_9",
+	["}"]  = "debug_skin",
 
 }
 

src/squeezeplay/share/jive/JiveMain.lua
@@ -59,6 +59,8 @@ local Task          = require("jive.ui.Task")
 local Timer         = require("jive.ui.Timer")
 local Event         = require("jive.ui.Event")
 
+local Canvas        = require("jive.ui.Canvas")
+
 local _inputToActionMap = require("jive.InputToActionMap")
 
 local debug         = require("jive.utils.debug")
@@ -258,6 +260,7 @@ function JiveMain:__init()
 
 --	profiler.stop()
 end
+
 
 function JiveMain:jiveMainNodes(globalStrings)
 

src/squeezeplay/share/jive/ui/Canvas.lua
@@ -1,0 +1,41 @@
+
+--[[
+=head1 NAME
+
+jive.ui.Canvas - A canvas widget
+
+=head1 DESCRIPTION
+
+A canvas widget, this widget provides access to drawing on the screen
+in lua.
+
+--]]
+
+-- stuff we use
+local _assert, tostring, type, tolua = _assert, tostring, type, tolua
+
+local oo        = require("loop.simple")
+local Icon      = require("jive.ui.Icon")
+local Surface   = require("jive.ui.Surface")
+
+local log       = require("jive.utils.log").logger("ui")
+
+
+-- our class
+module(...)
+oo.class(_M, Icon)
+
+
+function __init(self, style, renderFunc)
+	_assert(type(renderFunc) == "function")
+
+	local obj = oo.rawnew(self, Icon(style))
+	obj.render = renderFunc
+
+	return obj
+end
+
+
+function draw(self, surface)
+	self.render(surface)
+end

src/squeezeplay/share/jive/ui/Framework.lua
@@ -399,7 +399,7 @@ Remove the global widget I<widget> from the screen.
 function removeWidget(self, widget)
 	_assert(oo.instanceof(widget, Widget))
 
-	table.delete(widget, widget)
+	table.delete(widgets, widget)
 	widget:dispatchNewEvent(EVENT_HIDE)
 
 	self:reDraw(nil)

src/squeezeplay/src/ui/jive_window.c
@@ -134,6 +134,19 @@ int jiveL_window_iterate(lua_State *L) {
 	 * 2: closure
 	 */
 
+	// window widgets
+	lua_getfield(L, 1, "widgets");
+	lua_pushnil(L);
+	while (lua_next(L, -2) != 0) {
+		lua_pushvalue(L, 2);
+		lua_pushvalue(L, -2);
+		lua_call(L, 1, 1);
+
+		r = r | luaL_optinteger(L, -1, 0);
+		lua_pop(L, 2);
+	}
+	lua_pop(L, 1);
+
 	lua_getfield(L, 1, "showFrameworkWidgets");
 	if (lua_toboolean(L, -1)) {
 		// global widgets
@@ -148,19 +161,6 @@ int jiveL_window_iterate(lua_State *L) {
 			r = r | luaL_optinteger(L, -1, 0);
 			lua_pop(L, 2);
 		}
-		lua_pop(L, 2);
-	}
-	lua_pop(L, 1);
-
-	// window widgets
-	lua_getfield(L, 1, "widgets");
-	lua_pushnil(L);
-	while (lua_next(L, -2) != 0) {
-		lua_pushvalue(L, 2);
-		lua_pushvalue(L, -2);
-		lua_call(L, 1, 1);
-
-		r = r | luaL_optinteger(L, -1, 0);
 		lua_pop(L, 2);
 	}
 	lua_pop(L, 1);

commit 63356390748bc72be67cb4b172c4c29a219bbcd5

Fix layout issue introduced in r4248.
git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/trunk/squeezeplay@4268 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

src/squeezeplay/share/jive/ui/SimpleMenu.lua
@@ -130,7 +130,7 @@ local function _itemRenderer(menu, list, widgetList, indexList, size)
 			else
 				widgetList[i]:setStyle(item.style or "item")
 				widgetList[i]:setWidgetValue("text", item.text)
-				widgetList[i]:setWidget(iconStyle, icon)
+				widgetList[i]:setWidget("icon", icon)
 			end
 		end
 	end