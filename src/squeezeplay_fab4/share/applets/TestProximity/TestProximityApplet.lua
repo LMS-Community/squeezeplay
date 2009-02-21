ba8a35baac53afc57c3b75de6012d3c5c76a2238
Raphael Juchli <rjuchli@slimdevices.com>
2009-02-21 09:53:58 +0000

    Added outputs for density, proximity control and duty cycles to the proximity test applet Proximity Control and Duty Cycles can be directly configured and tested from the applet
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/private-branches/fab4/squeezeplay@4402 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay_fab4/share/applets/TestProximity/TestProximityApplet.lua
+++ src/squeezeplay_fab4/share/applets/TestProximity/TestProximityApplet.lua
@@ -12,6 +12,8 @@ local Font                   = require("jive.ui.Font")
 local Label                  = require("jive.ui.Label")
 local Menu                   = require("jive.ui.Menu")
 local SimpleMenu             = require("jive.ui.SimpleMenu")
+local RadioGroup             = require("jive.ui.RadioGroup")
+local RadioButton            = require("jive.ui.RadioButton")
 local Window                 = require("jive.ui.Window")
 
 local EVENT_SWITCH           = jive.ui.EVENT_SWITCH
@@ -21,10 +23,111 @@ local SW_TABLET_MODE	     = 1
 
 local log                    = require("jive.utils.log").addCategory("test", jive.utils.log.DEBUG)
 
-local menuItem = nil
+local SYSPATH = "/sys/bus/i2c/devices/0-0047/"
+
+-- Main Menu
+local mainMenu = nil
+local menuItemDetected = nil
+local menuItemDensity = nil
+local menuItemControl = nil
+local menuItemDutyCycle = nil
+
+-- store current values for config menus
+local curControl = 3
+local curDutyCycle = 4
 
 module(...)                                                                     
 oo.class(_M, Applet)
+
+function _updateLabel(self, menuItem, prefix, value)
+	menuItem.text = prefix .. value
+	mainMenu:updatedItem(menuItem)
+end
+
+function _update(self)
+
+	log:error("_update")
+
+	-- proximity_density
+	local f = io.open(SYSPATH .. "proximity_density")
+	local density = f:read("*all")
+	f:close()
+	self:_updateLabel(menuItemDensity, "Density: ", density)
+
+	-- control
+	f = io.open(SYSPATH .. "proximity_control")
+	local control = f:read("*all")
+	f:close()
+	curControl = tonumber(control)
+	self:_updateLabel(menuItemControl, "Control: ", curControl)
+
+	-- duty control
+	f = io.open(SYSPATH .. "proximity_duty_cycle")
+	local duty = f:read("*all")
+	f:close()
+	curDutyCycle = tonumber(duty)
+	self:_updateLabel(menuItemDutyCycle, "Duty Cycle: ", curDutyCycle)
+end
+
+function _changeControl(self, newValue)
+	-- Send The Command
+	local f = io.open(SYSPATH .. "proximity_control", "w")
+	f:write(tostring(newValue))
+	f:close()
+end
+
+function _changeDutyCycle(self, newValue)
+	-- Send The Command
+	local f = io.open(SYSPATH .. "proximity_duty_cycle", "w")
+	f:write(tostring(newValue))
+	f:close()
+end
+
+function menuSetControl(self)
+	local window = Window("window", "Change Control...")
+	local group = RadioGroup()
+
+	local menu = SimpleMenu("menu") 
+
+	for i = 0,3,1 do
+
+		menuItem = {
+                        text = tostring(i),
+                        icon = RadioButton("radio", group, function(event, menuItem)
+					self:_changeControl(i)					          
+                                end,                                                
+                        curControl == i)
+                }
+
+		menu:addItem(menuItem)
+ 	end
+
+	window:addWidget(menu)	
+        self:tieAndShowWindow(window)
+end
+
+function menuSetDutyCycle(self)
+	local window = Window("window", "Change Duty Cycle...")
+	local group = RadioGroup()
+
+        local menu = SimpleMenu("menu") 
+
+	for i = 0,7,1 do
+
+		menuItem = {
+                        text = tostring(i),
+                        icon = RadioButton("radio", group, function(event, menuItem)
+					self:_changeDutyCycle(i)					          
+                                end,                                                
+                        curDutyCycle == i)
+                }		
+	
+		menu:addItem(menuItem)
+	end
+
+	window:addWidget(menu)	
+        self:tieAndShowWindow(window)
+end
 
 function openWindow(self)
 	local window = Window("window", 'FactoryTest: Proximity')
@@ -32,12 +135,32 @@ function openWindow(self)
 
 	mainMenu = SimpleMenu("menu")
 
-	menuItem = {
+	menuItemDetected = {
 		text = "Proximity: None Detected"
 	}
 
+	menuItemDensity = {
+		text = "Density: n/a"
+	}
 
-	mainMenu:addItem(menuItem)
+	menuItemControl = {
+		text = "Control: n/a",
+		callback = function(event, index) 
+			self:menuSetControl()
+			end
+	}
+
+	menuItemDutyCycle =  {
+		text = "Duty Cycle: n/a",
+		callback = function(event, index) 
+			self:menuSetDutyCycle()
+			end
+	}
+
+	mainMenu:addItem(menuItemDetected)
+	mainMenu:addItem(menuItemDensity)
+	mainMenu:addItem(menuItemControl)
+	mainMenu:addItem(menuItemDutyCycle)
 	window:addWidget(mainMenu)
 
 	window:setAllowScreensaver(false)
@@ -49,14 +172,21 @@ function openWindow(self)
 
 			if( sw == SW_TABLET_MODE ) then 
 				if( val == 1 ) then
-					menuItem.text = "Proximity: *Detected*"
+					menuItemDetected.text = "Proximity: *Detected*"
 				else 
-					menuItem.text = "Proximity: None Detected";
+					menuItemDetected.text = "Proximity: None Detected";
 				end
 			end
 
 			mainMenu:updatedItem(menuItem)
                 end)
+
+	window:addAnimation(
+		function()
+                        self:_update()
+                end,
+		1
+        )
 
         self:tieAndShowWindow(window)
         return window
