603bcc058d329745e46146904484834c77482f6e
Richard Titmuss <rtitmuss@logitech.com>
2009-03-02 16:42:38 +0000

    r4396@harrypotter (orig r4395):  richard | 2009-02-20 12:44:11 +0000  Bug: N/A  Description:  Yet Another Skin Test Applet!
     This has been written in a clean style, to serve as a good example of how the window 
     styles should be used. At the moment it only covers the setup screens. To make it easier 
     for testing, fwd/rew move throught the screens and pause takes a screenshot. We should 
     probably also add shortcuts to cycle through the skins.
     
     
    
    
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/private-branches/fab4/squeezeplay@4553 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

---
+++ src/squeezeplay_test/share/applets/SkinTest/SkinTestApplet.lua
@@ -0,0 +1,439 @@
+--[[
+
+Skin test applet.
+
+Each window style is represented once in this applet. Every good rule needs
+an exception, so multiple keyboards are included :).
+
+To make it easier to test the following shortcuts can be used:
+	jump_fwd (b) - move to next screen
+	jump_rew (z) - move to previous screen
+	pause (c) - take screenshot
+
+
+The objective is to keep the reference code as simple as possible!
+
+--]]
+
+local ipairs = ipairs
+
+local oo                  = require("loop.simple")
+
+local Applet              = require("jive.Applet")
+local Framework           = require("jive.ui.Framework")
+local Icon                = require("jive.ui.Icon")
+local Label               = require("jive.ui.Label")
+local Popup               = require("jive.ui.Popup")
+local Keyboard            = require("jive.ui.Keyboard")
+local SimpleMenu          = require("jive.ui.SimpleMenu")
+local Slider              = require("jive.ui.Slider")
+local Surface             = require("jive.ui.Surface")
+local Textarea            = require("jive.ui.Textarea")
+local Textinput           = require("jive.ui.Textinput")
+local Window              = require("jive.ui.Window")
+
+local debug               = require("jive.utils.debug")
+local log                 = require("jive.utils.log").addCategory("test", jive.utils.log.DEBUG)
+
+
+module(..., Framework.constants)
+oo.class(_M, Applet)
+
+
+local windows -- defined at end
+local testData -- defined at end
+
+
+function _itemName(item)
+	return item[2] .. " (" .. item[1] .. ")"
+end
+
+
+function _itemData(item)
+	return testData[item[1]]
+end
+
+
+-- move to the next text window
+function _windowNext(self, item)
+	local next
+
+	for i,anitem in ipairs(windows) do
+		if item == anitem then
+			next = i + 1
+		end
+	end
+
+	if next > #windows then
+		next = 1
+	end
+
+	windows[next][3](self, windows[next]):showInstead()
+end
+
+
+-- move to the next previous window
+function _windowPrev(self, item)
+	local prev
+
+	for i,anitem in ipairs(windows) do
+		if item == anitem then
+			prev = i - 1
+		end
+	end
+
+	if prev < 1 then
+		prev = #windows
+	end
+
+	windows[prev][3](self, windows[prev]):showInstead()
+end
+
+
+-- add test actions to window
+function _windowActions(self, item, window)
+	window:addActionListener("jump_fwd",
+		nil,
+		function()
+			_windowNext(self, item)
+		end)
+
+	window:addActionListener("jump_rew",
+		nil,
+		function()
+			_windowPrev(self, item)
+		end)
+
+	window:addActionListener("pause",
+		nil,
+		function()
+			local sw, sh = Framework:getScreenSize()
+			local img = Surface:newRGB(sw, sh)
+
+			Framework:draw(img)
+
+			log:info("Saving ", item[1] .. ".bmp")
+			img:saveBMP(item[1] .. ".bmp")
+		end)
+end
+
+
+-- top level menu
+function menu(self)
+	local window = Window("window", "Skin Tests")
+
+	local menu = SimpleMenu("menu")
+
+	for i,item in ipairs(windows) do
+		menu:addItem({
+			text = _itemName(item),
+			sound = "WINDOWSHOW",
+			callback = function()
+				item[3](self, item):show()
+			end,
+		})
+	end
+
+	window:addWidget(menu)
+	self:tieAndShowWindow(window)
+end
+
+
+-- this is a dummy help window
+function dummy_help(self)
+	local window = Window("help", "Help")
+	window:setAllowScreensaver(false)
+
+	local textarea = Textarea("text", "This is a help window.")
+	window:addWidget(textarea)
+	self:tieAndShowWindow(window)
+end
+
+
+
+-- REFERENCE WINDOW STYLES ARE BELOW
+
+
+
+--[[
+Window:   "setup"
+Menu:     "menu"
+Item:     "item", "itemchecked"
+--]]
+function setup_window(self, item)
+	local data = _itemData(item)
+
+	local window = Window("setup", _itemName(item), "setup")
+	_windowActions(self, item, window)
+
+	local selected = nil
+
+	local menu = SimpleMenu("menu")
+	for i,text in ipairs(data[1]) do
+		menu:addItem({
+			text = text,
+			sound = "WINDOWSHOW",
+			callback = function(event, item)
+				if selected then
+					selected.style = "item"
+					menu:updatedItem(selected)
+				end
+				item.style = "itemchecked"
+				menu:updatedItem(item)
+
+				selected = item
+			end,
+		})		
+	end
+	window:addWidget(menu)
+
+	self:tieWindow(window)
+	return window
+end
+
+
+--[[
+Window:   "onebutton"
+Textarea: "text"
+Menu:     "menu"
+Item:     "item"
+--]]
+function setup_onebutton(self, item)
+	local data = _itemData(item)
+
+	local window = Window("onebutton", _itemName(item), "setup")
+	_windowActions(self, item, window)
+
+	local textarea = Textarea("text", data[1])
+
+	local menu = SimpleMenu("menu")
+	menu:addItem({
+		text = data[2]
+	})
+
+	window:addWidget(textarea)
+	window:addWidget(menu)
+
+	self:tieWindow(window)
+	return window
+end
+
+
+--[[
+Window:   "button"
+Textarea: "text"
+Menu:     "menu"
+Item:     "item"
+--]]
+function setup_button(self, item)
+	local data = _itemData(item)
+
+	local window = Window("button", _itemName(item), "setup")
+	_windowActions(self, item, window)
+
+	window:addActionListener("help", self, dummy_help)
+	window:setButtonAction("rbutton", "help")
+
+	local textarea = Textarea("text", data[1])
+
+	local menu = SimpleMenu("menu")
+	for i,subdata in ipairs(data[2]) do
+		menu:addItem({
+			text = subdata[1],
+			iconStyle = subdata[2],
+		})
+	end
+
+	window:addWidget(textarea)
+	window:addWidget(menu)
+
+	self:tieWindow(window)
+	return window
+end
+
+
+--[[
+Window:   "help"
+Textarea: "text"
+--]]
+function setup_help(self, item)
+	local data = _itemData(item)
+
+	local window = Window("help", _itemName(item), "setup")
+	_windowActions(self, item, window)
+
+	local textarea = Textarea("text", data[1])
+
+	window:addWidget(textarea)
+
+	self:tieWindow(window)
+	return window
+end
+
+
+--[[
+Window:   "waiting"
+Label:    "text", "subtext"
+Icon:     X
+--]]
+function setup_waiting(self, item)
+	local data = _itemData(item)
+
+	local popup = Popup("waiting", _itemName(item))
+	_windowActions(self, item, popup)
+
+	local label = Label("text", data[1])
+	local sublabel = Label("text", data[2])
+	local icon = Icon(data[3])
+
+	popup:addWidget(label)
+	popup:addWidget(icon)
+	popup:addWidget(sublabel)
+
+	self:tieWindow(popup)
+	return popup
+end
+
+
+--[[
+Window:    "input"
+Textinput: "textinput"
+Keyboard:  "keyboard"
+--]]
+function setup_keyboard(self, item)
+	local data = _itemData(item)
+
+	local window = Window("input", _itemName(item), "setup")
+	_windowActions(self, item, window)
+
+	-- normal short cuts don't work with text entry
+	window:addActionListener("back", nil, function()
+		_windowPrev(self, item)
+	end)
+
+	local textinput = Textinput("textinput", data[1],
+		function(_, value)
+			_windowNext(self, item)
+		end)
+
+	window:addWidget(textinput)
+	window:addWidget(Keyboard("keyboard", data[2]))
+
+	window:focusWidget(textinput)
+
+	return window
+end
+
+
+--[[
+Window:    "error"
+Textarea:  "text"
+Menu:      "menu"
+Item:      "item"
+--]]
+function setup_error(self, item)
+	local data = _itemData(item)
+
+	local window = Window("error", _itemName(item), "setup")
+	_windowActions(self, item, window)
+
+	local textarea = Textarea("text", data[1])
+
+	local menu = SimpleMenu("menu")
+	for i,data in ipairs(data[2]) do
+		menu:addItem({
+			text = data,
+		})
+	end
+
+	window:addWidget(textarea)
+	window:addWidget(menu)
+
+	self:tieWindow(window)
+	return window
+end
+
+
+--[[
+Window:   "update"
+Label:    "text"
+Slider:   "progress"
+Icon:     X
+--]]
+function setup_update(self, item)
+	local data = _itemData(item)
+
+	local popup = Popup("update", _itemName(item))
+	_windowActions(self, item, popup)
+
+	local textarea = Textarea("text", data[1])
+	local icon = Icon(data[2])
+	local progress = Slider("progress", 1, 100, 1)
+
+	local count = 1
+	popup:addTimer(500, function()
+		count = count + 1
+		progress:setRange(1, 100, count)
+	end)
+
+	popup:addWidget(textarea)
+	popup:addWidget(icon)
+	popup:addWidget(progress)
+
+	self:tieWindow(popup)
+	return popup
+end
+
+
+
+-- REFERENCE WINDOW STYLES ARE ABOVE
+
+
+
+
+-- the reference windows, and test data
+windows = {
+	{ "setup", "Languages", setup_window, },
+	{ "onebutton", "Welcome to Setup", setup_onebutton, },
+	{ "button", "Choose Region", setup_button, },
+	{ "help", "Help Connection Type", setup_help, },
+	{ "waiting", "Connecting to", setup_waiting, },
+	{ "keyboard_wpa", "Wireless Password", setup_keyboard, },
+	{ "error", "Error", setup_error, },
+	{ "update", "Software Update", setup_update, },
+}
+
+
+testData = {
+	setup = {
+		{ "Deutsch", "Suomi", "English", "Dansk", "Itailiano", "Français", "Norsk", "Sevnska", "Español" }
+	},
+	onebutton = {
+		"Let's begin by getting\nyou connected to your network.",
+		"Continue"
+	},
+	button = {
+		"Is text allowed in this window?",
+		{ { "North America", "region_US" },
+		  { "All Other Regions" , "region_XX" },
+		},
+	},
+	help = {
+		"This is some help text, in a help window. It could be very long, and may need a scrollbar.", -- todo add more text
+	},
+	waiting = {
+		"Connecting to\nwireless network...", "all your base", "iconConnecting",
+	},
+	keyboard_wpa = {
+		Textinput.textValue("", 8, 20), 'qwerty',
+	},
+	error = {
+		"DHCP Address Cannot be Found",
+		{ "Try to Detect Automatically",
+		  "Manually Enter IP Address",
+		},
+	},
+	update = {
+		"Installing\nSoftware Update...", "iconSoftwareUpdate",
+	},
+}
