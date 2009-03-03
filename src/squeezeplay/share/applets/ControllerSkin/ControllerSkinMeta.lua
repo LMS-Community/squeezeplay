7be2433cb5f3170c1f9ae355cc5ab30ac2e0caec
Tom Wadzinski <tom_wadzinski@logitech.com>
2009-03-03 19:49:07 +0000

    - Committing initial copy of fab4 remote skin, so I can see diffs as I go
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/private-branches/fab4/squeezeplay@4670 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

---
+++ src/squeezeplay/share/applets/ControllerSkin/ControllerSkinMeta.lua
@@ -0,0 +1,61 @@
+
+--[[
+=head1 NAME
+
+applets.TouchSkin.TouchSkinMeta - TouchSkin meta-info
+
+=head1 DESCRIPTION
+
+See L<applets.TouchSkin.TouchSkinApplet>.
+
+=head1 FUNCTIONS
+
+See L<jive.AppletMeta> for a description of standard applet meta functions.
+
+=cut
+--]]
+
+
+local oo            = require("loop.simple")
+
+local AppletMeta    = require("jive.AppletMeta")
+
+local appletManager = appletManager
+local jiveMain      = jiveMain
+local log           = require('jive.utils.log').logger('applets.misc')
+
+
+module(...)
+oo.class(_M, AppletMeta)
+
+
+function jiveVersion(self)
+	return 1, 1
+end
+
+function defaultSettings(self)
+        self.params = {
+		THUMB_SIZE = 56,
+		nowPlayingBrowseArtworkSize = 154,
+		nowPlayingSSArtworkSize     = 186,
+		nowPlayingLargeArtworkSize  = 240,
+        }
+	return self.params
+end
+
+function registerApplet(self)
+	jiveMain:registerSkin(self:string("CONTROLLER_SKIN") .. "TEMP", "ControllerSkin", "skin", self.params)
+end
+
+
+--[[
+
+=head1 LICENSE
+
+Copyright 2007 Logitech. All Rights Reserved.
+
+This file is subject to the Logitech Public Source License Version 1.0. Please see the LICENCE file for details.
+
+=cut
+--]]
+
