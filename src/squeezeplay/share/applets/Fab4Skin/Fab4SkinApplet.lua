c2c1c1612fa0b54922d48cce0c47dd6f247411e8
Ben Klaas <bklaas@logitech.com>
2009-03-12 23:58:49 +0000

    Description: sync assets with Noah fix margin of scrollbar and top menu item with titlebar
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/private-branches/fab4/squeezeplay@4774 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/share/applets/Fab4Skin/Fab4SkinApplet.lua
+++ src/squeezeplay/share/applets/Fab4Skin/Fab4SkinApplet.lua
@@ -404,7 +404,7 @@ function skin(self, s)
 
 	s.menu = {
 		position = LAYOUT_CENTER,
-		padding = { 0, 0, 0, 0 },
+		padding = { 0, -2, 0, 0 },
 		itemHeight = FIVE_ITEM_HEIGHT,
 		fg = {0xbb, 0xbb, 0xbb },
 		font = _boldfont(250),
@@ -565,8 +565,8 @@ function skin(self, s)
 	}
 
 	s.scrollbar = {
-		w = 34,
-		border = 0,
+		w = 32,
+		border = { -4, -1, 0, 0 },
 		padding = { 0, 24, 0, 22 },
 		horizontal = 0,
 		bgImg = scrollBackground,
