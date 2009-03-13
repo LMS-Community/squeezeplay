499dd62e96c239b22dbc5e22470b34c8e9f22a9e
Ben Klaas <bklaas@logitech.com>
2009-03-13 00:55:33 +0000

    Description: sync with Noah via my l33t new sync script also, checkin l33t new sync script
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/private-branches/fab4/squeezeplay@4779 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

---
+++ src/squeezeplay/share/applets/Fab4Skin/sync_assets.pl
@@ -0,0 +1,75 @@
+#!/usr/bin/perl
+
+#
+# Script to help intelligently copy and update png files from Fab4Skin assets area to skin
+#
+# 1. removes all png files from images/ directory, except those in the directory UNOFFICIAL
+# 2. copies every png file from assets/Fab4Skin/images area to correct path in images/ directory
+# 3. runs svk status, uses output to create a shell script for doing the necessary svk commands
+#
+# bklaas 03.09
+
+use strict;
+use File::Next;
+use File::Basename;
+use File::Copy;
+
+my $skinDir = "images/";
+my $assetDir = "../../../../../assets/Fab4Skin/images/";
+
+my $images = get_assets($skinDir);
+my $assets = get_assets($assetDir);
+
+# remove existing images
+remove_images($images);
+# copy assets
+copy_assets($assets);
+# create svk file
+create_svk_file();
+
+sub create_svk_file {
+	my $prog = "svk status $skinDir";
+	open(OUT, ">svkUpdate.bash");
+	open(PROG, "$prog |");
+	print OUT "#!/bin/bash\n\n";
+	while(<PROG>) {
+		s/^\?\s+/svk add /;
+		s/^\!\s+/svk remove /;
+		if (/^svk/) {
+			print OUT $_;
+		}
+	}
+	close(OUT);
+	close(PROG);
+}
+
+sub get_assets {
+	my $path = shift;
+	my $files = File::Next::files($path);
+	my @return;
+	while ( defined ( my $file = $files->() ) ) {
+		push @return, $file if $file =~ /\.png$/ && $file !~ /UNOFFICIAL/;
+	}
+	return \@return;
+}
+
+sub remove_images {
+	my $images = shift;
+	for my $image (@$images) {
+		my $success = unlink $image;
+		if ($success != 1) {
+			print STDERR "There was a problem removing $image\n";
+		}
+	}
+}
+
+sub copy_assets {
+	my $assets = shift;
+	for my $image (@$assets) {
+		my $newImage = $image;
+		$newImage =~ s/$assetDir/$skinDir/;
+		copy($image, $newImage);
+	}
+}
+
+
