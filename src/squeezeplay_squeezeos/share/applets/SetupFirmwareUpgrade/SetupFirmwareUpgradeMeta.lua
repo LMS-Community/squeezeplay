79ce53597d8ad43e664904065cc3e3d1aa5ab2b1
Richard Titmuss <rtitmuss@logitech.com>
2009-03-17 11:44:30 +0000

    Remove the subscription from the firmware upgrade applet, this is now done in the SlimServer object.
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/private-branches/fab4/squeezeplay@4818 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay_squeezeos/share/applets/SetupFirmwareUpgrade/SetupFirmwareUpgradeMeta.lua
+++ src/squeezeplay_squeezeos/share/applets/SetupFirmwareUpgrade/SetupFirmwareUpgradeMeta.lua
@@ -1,12 +1,8 @@
 
-local error, tonumber = error, tonumber
-
 local oo            = require("loop.simple")
 
 local AppletMeta    = require("jive.AppletMeta")
-local System        = require("jive.System")
 
-local jul           = require("jive.utils.log")
 local log           = require("jive.utils.log").logger("applets.setup")
 local debug         = require("jive.utils.debug")
 
@@ -14,15 +10,15 @@ local appletManager = appletManager
 local jiveMain      = jiveMain
 local jnt           = jnt
 
-local JIVE_VERSION  = jive.JIVE_VERSION
-
 -- naughty polluting global table, but I don't want to store this
 -- in the applet settings as this value does not need to be persisted
 upgradeUrl          = { false }
 local upgradeUrl    = upgradeUrl
 
+
 module(...)
 oo.class(_M, AppletMeta)
+
 
 function jiveVersion(meta)
 	return 1, 1
@@ -41,77 +37,20 @@ function registerApplet(meta)
 end
 
 
-function notify_playerDelete(meta, player)
-	if meta.player ~= player then
-		return
-	end
-
-	meta.player:unsubscribe('/slim/firmwarestatus/' .. meta.player:getId())
-	meta.player = false
-end
-
-
 function notify_playerCurrent(meta, player)
-	-- FIXME
-	-- if the player remains the same we need to resubscribe. This should
-	-- not be needed when the player subscription bug 8587 is fixed.
+	local server = player:getSlimServer()
 
-	if meta.player then
-		meta.player:unsubscribe('/slim/firmwarestatus/' .. meta.player:getId())
-	end
-
-	meta.player = player
-
-	if not player then
-		upgradeUrl = { false }
+	if not server then
 		return
 	end
-	
-	local firmwareUpgradeSink =
-		function(chunk, err)
-			if err then
-				log:warn(err)
-				return
-			end
-			
-			if not meta.player or not meta.player:getSlimServer() then
-				log:warn("Firmware upgrade response but not connected to server")
-				return
-			end
 
-			-- store firmware upgrade url
-			-- Bug 6828, use a relative URL from SC to handle dual-homed servers
-			if chunk.data.relativeFirmwareUrl then
-				local ip, port = meta.player:getSlimServer():getIpPort()
-				upgradeUrl[1] = 'http://' .. ip .. ':' .. port .. chunk.data.relativeFirmwareUrl
-				log:info("Relative Firmware URL=", upgradeUrl[1])
-			elseif chunk.data.firmwareUrl then
-				upgradeUrl[1] = chunk.data.firmwareUrl
-				log:info("Firmware URL=", upgradeUrl[1])
-			end
+	local url, force = server:getUpgradeUrl()
+	upgradeUrl[1] = url
 
-			-- are we offering or forcing an upgrade
-			if tonumber(chunk.data.firmwareUpgrade) == 1 then
-				log:info("Firmware upgrade")
-				appletManager:callService("forceUpgrade", tonumber(chunk.data.firmwareOptional) == 1, upgradeUrl[1], chunk.data.firmwareHelp)
-
-				meta.player:unsubscribe('/slim/firmwarestatus/' .. meta.player:getId())
-			end
-
-		end
-
-	local fwcmd = {
-		'firmwareupgrade',
-		'firmwareVersion:' .. JIVE_VERSION,
-		'machine:' .. System:getMachine(),
-		'subscribe:0'
-	}
-	player:subscribe(
-			 '/slim/firmwarestatus/' .. player:getId(),
-			 firmwareUpgradeSink,
-			 player:getId(),
-			 fwcmd
-		 )
+	if force then
+		log:info("Firmware upgrade")
+		appletManager:callService("forceUpgrade", force, url)
+	end
 end
 
 
