893bc18e9b81d50abd22faf6949a8d214783c6ee
Ben Klaas <bklaas@logitech.com>
2009-04-05 16:04:28 +0000

    Description: I may have been overzealous in removing constant declarations, as these don't appear in Framework.lua:constants putting them back in for now...
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/private-branches/fab4/squeezeplay@5181 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/share/applets/ControllerSkin/ControllerSkinApplet.lua
+++ src/squeezeplay/share/applets/ControllerSkin/ControllerSkinApplet.lua
@@ -41,6 +41,16 @@ local autotable              = require("jive.utils.autotable")
 
 local log = require("jive.utils.log").logger("ui")
 
+local LAYER_FRAME            = jive.ui.LAYER_FRAME
+local LAYER_CONTENT_ON_STAGE = jive.ui.LAYER_CONTENT_ON_STAGE
+
+local LAYOUT_NORTH           = jive.ui.LAYOUT_NORTH
+local LAYOUT_EAST            = jive.ui.LAYOUT_EAST
+local LAYOUT_SOUTH           = jive.ui.LAYOUT_SOUTH
+local LAYOUT_WEST            = jive.ui.LAYOUT_WEST
+local LAYOUT_CENTER          = jive.ui.LAYOUT_CENTER
+local LAYOUT_NONE            = jive.ui.LAYOUT_NONE
+
 local WH_FILL                = jive.ui.WH_FILL
 
 local jiveMain               = jiveMain
