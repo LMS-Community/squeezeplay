d4510756961192b7ae9ad0118271ae3c3a4344f5
Richard Titmuss <rtitmuss@logitech.com>
2009-04-20 19:38:25 +0000

    r5376@harrypotter (orig r5375):  richard | 2009-04-20 20:16:12 +0100  Bug: N/A  Description:  These mode lines are not needed, and actually break some displays.
     r5381@harrypotter (orig r5380):  richard | 2009-04-20 20:37:05 +0100
      r5377@harrypotter (orig r5376):  richard | 2009-04-20 20:18:21 +0100
      Bug: 8984
      Description:
      Fix dhcp problem with Microsoft router.
      
      
     
    
    
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/branches/pango/squeezeplay@5381 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/SDL-1.2.13/src/video/fbcon/SDL_fbvideo.c
+++ src/SDL-1.2.13/src/video/fbcon/SDL_fbvideo.c
@@ -97,7 +97,6 @@ static const struct {
 	int vmode;
 } vesa_timings[] = {
 #ifdef USE_VESA_TIMINGS	/* Only tested on Matrox Millenium I */
-	{  240,  320, 287820000,  2, 2, 2,  1,  2, 2, 0, 0 },	/* PAD LCD */
 	{  640,  400, 39771,  48, 16, 39,  8,  96, 2, 2, 0 },	/* 70 Hz */
 	{  640,  480, 39683,  48, 16, 33, 10,  96, 2, 0, 0 },	/* 60 Hz */
 	{  768,  576, 26101, 144, 16, 28,  6, 112, 4, 0, 0 },	/* 60 Hz */
@@ -113,7 +112,6 @@ static const struct {
 	   the 'modeline2fb' perl script included with the fbset package.
 	   These timings were generated for Matrox Millenium I, 15" monitor.
 	*/
-	{  240,  320, 287820000,  2, 2, 2,  1,  2, 2, 0, 0 },	/* PAD LCD */
 	{  320,  200, 79440,  16, 16, 20,  4,  48, 1, 0, 2 },	/* 70 Hz */
 	{  320,  240, 63492,  16, 16, 16,  4,  48, 2, 0, 2 },	/* 72 Hz */
 	{  512,  384, 49603,  48, 16, 16,  1,  64, 3, 0, 0 },	/* 78 Hz */
