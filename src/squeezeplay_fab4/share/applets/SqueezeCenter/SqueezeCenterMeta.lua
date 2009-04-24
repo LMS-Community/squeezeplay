05893e28116886e017e3612226c17d727d969d49
Alan Young <ayoung1@logitech.com>
2009-04-24 16:36:55 +0000

    First version of SqueezeCenter control applet, Does not dynamically enable/disable the individual menu items yet Does not set up a timer to keep the status up to date during the period that the applet is active
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/private-branches/fab4/squeezeplay@5474 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

---
+++ src/squeezeplay_fab4/share/applets/SqueezeCenter/SqueezeCenterMeta.lua
@@ -0,0 +1,35 @@
+
+local oo            = require("loop.simple")
+
+local AppletMeta    = require("jive.AppletMeta")
+local jul           = require("jive.utils.log")
+
+local appletManager = appletManager
+local jiveMain      = jiveMain
+
+
+module(...)
+oo.class(_M, AppletMeta)
+
+
+function jiveVersion(meta)
+	return 1, 1
+end
+
+
+function registerApplet(meta)
+	jiveMain:addItem(meta:menuItem('appletSqueezeCenter', 'advancedSettings', "SqueezeCenter", function(applet, ...) applet:settingsShow(...) end))
+end
+
+
+--[[
+
+=head1 LICENSE
+
+Copyright 2007 Logitech. All Rights Reserved.
+
+This file is subject to the Logitech Public Source License Version 1.0. Please see the LICENCE file for details.
+
+=cut
+--]]
+
