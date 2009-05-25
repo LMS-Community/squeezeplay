7d6ff8abd854fdf21f6bf0a98606b026bc4aaec4
Ben Klaas <bklaas@logitech.com>
2009-05-25 19:04:43 +0000

    Description: remove unnecessary block
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/private-branches/fab4/squeezeplay@5862 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/share/applets/NowPlaying/NowPlayingApplet.lua
+++ src/squeezeplay/share/applets/NowPlaying/NowPlayingApplet.lua
@@ -174,18 +174,6 @@ function notify_playerTrackChange(self, player, nowPlaying)
 	self.player = player
 	local playerStatus = player:getPlayerStatus()
 
-	if playerStatus and 
-		playerStatus.remoteMeta and 
-		playerStatus.remoteMeta.buttons 
-	then
-		-- disable rew or fw as needed
-		if playerStatus.rew and playerStatus.rew == 0 then
-		else
-		end
-		if playerStatus.ffw and playerStatus.ffw == 0 then
-		else
-		end
-	end
 	-- create the window to display
 	local window = _createUI(self)
 	if self.window then
