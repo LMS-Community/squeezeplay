6790316930b88989bfcaeb67d4b5099e389a30b5
Richard Titmuss <rtitmuss@logitech.com>
2009-08-10 19:37:52 +0000

    Mac address and uptime are not meant to be in About menu, they are now in Diagnostics.
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/private-branches/fab4/squeezeplay@6993 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/share/applets/AboutJive/AboutJiveApplet.lua
+++ src/squeezeplay/share/applets/AboutJive/AboutJiveApplet.lua
@@ -32,26 +32,9 @@ function settingsShow(self)
 
 	local version = JIVE_VERSION
 
-	local uptime = System:getUptime()
-
-	local ut = {}
-	if uptime.days > 0 then
-		ut[#ut + 1] = tostring(self:string("UPTIME_DAYS", uptime.days))
-	end
-	if uptime.hours > 0 then
-		ut[#ut + 1] = tostring(self:string("UPTIME_HOURS", uptime.hours))
-	end
-	ut[#ut + 1] = tostring(self:string("UPTIME_MINUTES", uptime.minutes))
-	ut = table.concat(ut, " ")
-
 	local about = {
 		tostring(self:string("ABOUT_VERSION")),
 		version,
-		"",
-		tostring(self:string("ABOUT_MAC_ADDRESS", System:getMacAddress()) or ""),
-		"",
-		tostring(self:string("UPTIME")),
-		ut,
 		"",
 		tostring(self:string("ABOUT_CREDITS")),
 		"     Sean Adams",
