LMS-Community/squeezeplay

commit 05d0da9e27ecb3f321920677fb26134ee9d121d6

Don't do a wireless scan when looking for servers, only when looking for players (when we need to find Receivers in setup mode).
Fixed bug #12150

git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/private-branches/fab4/squeezeplay@7143 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

src/squeezeplay/share/applets/SlimDiscovery/SlimDiscoveryApplet.lua
@@ -12,10 +12,13 @@ connected: we are connected to a player, only a connection to our
  SC (or SN) is maintained. udp scanning is still performed in the
  background to update the SqueezeCenter list.
 
-probing: we are connected to a player, but we must probe all SC/SN
+probing_player: we are connected to a player, but we must probe all SC/SN
  update our internal state. this is used for example in the choose
  player screen.
 
+probing_server: we are connected to a player, but we must probe all SC/SN
+ update our internal state. this is used for example in the choose
+ server screen. we do not need to find players via udap or wireless scanning.
 
 --]]
 
@@ -124,7 +127,8 @@ function _serverUpdateAddress(self, server, ip, port, name)
 	server:updateAddress(ip, port, name)
 
 	if self.state == 'searching'
-		or self.state == 'probing' then
+		or self.state == 'probing_player'
+		or self.state == 'probing_server' then
 
 		-- connect to server when searching or probing
 		server:connect()
@@ -253,17 +257,18 @@ function _discover(self)
 		self.socket:send(_slimDiscoverySource, address, PORT)
 	end
 
-	-- UDAP discovery
-	local packet = Udap.createAdvancedDiscover(nil, 1)
-	log:debug("sending udap discovery to 255.255.255.255")
-	self.udap:send(function() return packet end, "255.255.255.255")
+	-- Discover players via udap or wireless scanning
+	if self.state == 'probing_player' then
+		-- UDAP discovery
+		local packet = Udap.createAdvancedDiscover(nil, 1)
+		log:debug("sending udap discovery to 255.255.255.255")
+		self.udap:send(function() return packet end, "255.255.255.255")
 
-	-- Wireless discovery, only in probing state
-	if self.state == 'probing' and self.wireless then
-		self.wireless:scan(
-			function(scanTable)
+		if self.wireless then
+			self.wireless:scan(function(scanTable)
 				_scanComplete(self, scanTable)
 			end)
+		end
 	end
 
 
@@ -280,7 +285,7 @@ function _discover(self)
 	_playerCleanup(self)
 
 
-	if self.state == 'probing' and
+	if self.state == 'probing_player' or self.state == 'probing_server' and
 		Framework:getTicks() > self.probeUntil then
 
 		local currentPlayer = Player:getCurrentPlayer()
@@ -328,7 +333,7 @@ function _setState(self, state)
 	elseif state == 'connected' then
 		self:_idleDisconnect()
 
-	elseif state == 'probing' then
+	elseif state == 'probing_player' or state == 'probing_server' then
 		self.probeUntil = Framework:getTicks() + 60000
 		self.timer:restart(0)
 		self:_connect()
@@ -581,7 +586,12 @@ end
 
 
 function discoverPlayers(self)
-	self:_setState("probing")
+	self:_setState("probing_player")
+end
+
+
+function discoverServers(self)
+	self:_setState("probing_server")
 end
 
 

src/squeezeplay/share/applets/ChooseMusicSource/ChooseMusicSourceApplet.lua
@@ -127,8 +127,8 @@ function _showMusicSourceList(self)
 
 
 	-- Discover players in this window
-	appletManager:callService("discoverPlayers")
-	window:addTimer(1000, function() appletManager:callService("discoverPlayers") end)
+	appletManager:callService("discoverServers")
+	window:addTimer(1000, function() appletManager:callService("discoverServers") end)
 
 
 	-- squeezecenter on the poll list
@@ -541,7 +541,7 @@ function _showConnectToServer(self, player, server)
 		window:addTimer(1000,
 				function()
 					-- scan all servers waiting for the player
-					appletManager:callService("discoverPlayers")
+					appletManager:callService("discoverServers")
 
 					-- we detect when the connect to the new server
 					-- with notify_playerNew

src/squeezeplay/share/applets/SlimDiscovery/SlimDiscoveryMeta.lua
@@ -49,6 +49,7 @@ function registerApplet(meta)
 	meta:registerService("setCurrentPlayer")
 
 	meta:registerService("discoverPlayers")
+	meta:registerService("discoverServers")
 
 	meta:registerService("connectPlayer")
 	meta:registerService("disconnectPlayer")

commit f1e7b507099d809a4347cf6f242885540d77933d

- Fixed regression that occurred in r7143
Fixed Bug:13524

git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/private-branches/fab4/squeezeplay@7238 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

src/squeezeplay/share/applets/SlimDiscovery/SlimDiscoveryApplet.lua
@@ -258,12 +258,12 @@ function _discover(self)
 	end
 
 	-- Discover players via udap or wireless scanning
-	if self.state == 'probing_player' then
-		-- UDAP discovery
-		local packet = Udap.createAdvancedDiscover(nil, 1)
-		log:debug("sending udap discovery to 255.255.255.255")
-		self.udap:send(function() return packet end, "255.255.255.255")
+	-- UDAP discovery
+	local packet = Udap.createAdvancedDiscover(nil, 1)
+	log:debug("sending udap discovery to 255.255.255.255")
+	self.udap:send(function() return packet end, "255.255.255.255")
 
+	if self.state == 'probing_player' then
 		if self.wireless then
 			self.wireless:scan(function(scanTable)
 				_scanComplete(self, scanTable)