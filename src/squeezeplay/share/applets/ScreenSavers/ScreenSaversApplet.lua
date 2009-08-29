7473a30ff7802af4485ac1380a054e25040851ad
Tom Wadzinski <tom_wadzinski@logitech.com>
2009-08-29 16:43:37 +0000

    power on window works again with blank screen ss Fixed Bug:13731
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/private-branches/fab4/squeezeplay@7323 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/share/applets/ScreenSavers/ScreenSaversApplet.lua
+++ src/squeezeplay/share/applets/ScreenSavers/ScreenSaversApplet.lua
@@ -410,7 +410,7 @@ function _showPowerOnWindow(self)
 		end
 
 		local instance = appletManager:loadApplet(screensaver.applet)
-		if instance.usePowerOnWindow and instance:usePowerOnWindow() then
+		if instance.usePowerOnWindow and not instance:usePowerOnWindow() then
 			log:debug("ss: don't use power on window")
 			return
 		end
