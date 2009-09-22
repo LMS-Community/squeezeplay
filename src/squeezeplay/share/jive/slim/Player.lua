eee21c4a7cdfe38d930a30d6c02784b6883a759a
Richard Titmuss <rtitmuss@logitech.com>
2009-09-22 20:30:38 +0000

    Andy changed his mind, the playerRegister is needed even without a uuid.
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/branches/7.4.0/squeezeplay@7711 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/share/jive/slim/Player.lua
+++ src/squeezeplay/share/jive/slim/Player.lua
@@ -1762,12 +1762,10 @@ function _udapConnect(self, server)
 		data.slimserver_address = Udap.packNumber(parseip("0.0.0.1"), 4)
 
 		-- make sure the player is linked on SN
-		if self.uuid then
-			log:info("linking player ", self.id, " on SN")
-			server:request(nil, nil, {
-				'playerRegister', self.uuid, self.id, self.info.name
-			})
-		end
+		log:info("linking player ", self.id, " on SN")
+		server:request(nil, nil, {
+			'playerRegister', self.uuid, self.id, self.info.name
+		})
 	else
 		local serverip = server:getIpPort()
 
