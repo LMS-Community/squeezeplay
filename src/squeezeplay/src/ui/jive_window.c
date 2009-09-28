f3e1d071fd19e3675decb9819918406f4b64ca7d
Tom Wadzinski <tom_wadzinski@logitech.com>
2009-09-28 18:17:55 +0000

    - ir events were being sent to all widgets not just the focused widget. Bug: 14288
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/trunk/squeezeplay@7792 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/src/ui/jive_window.c
+++ src/squeezeplay/src/ui/jive_window.c
@@ -338,6 +338,11 @@ int jiveL_window_event_handler(lua_State *L) {
 	case JIVE_EVENT_KEY_UP:
 	case JIVE_EVENT_KEY_PRESS:
 	case JIVE_EVENT_KEY_HOLD:
+	case JIVE_EVENT_IR_PRESS:
+	case JIVE_EVENT_IR_HOLD:
+	case JIVE_EVENT_IR_UP:
+	case JIVE_EVENT_IR_DOWN:
+	case JIVE_EVENT_IR_REPEAT:
 	case JIVE_ACTION:
 
 		/*
