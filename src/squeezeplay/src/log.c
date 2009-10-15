d5147aec9e57f50a6fb5451b61db53131bf6d6d7
Felix Mueller <fmueller@logitech.com>
2009-10-15 08:13:24 +0000

    Description: Fix line endings and add new files to project
    Bug: 14778
    
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.5/trunk/squeezeplay@7859 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/src/log.c
+++ src/squeezeplay/src/log.c
@@ -50,7 +50,7 @@ struct timezone
   int  tz_minuteswest; /* minutes W of Greenwich */
   int  tz_dsttime;     /* type of dst correction */
 };
- 
+
 int gettimeofday(struct timeval *tv, struct timezone *tz)
 {
   FILETIME ft;
@@ -60,18 +60,18 @@ int gettimeofday(struct timeval *tv, struct timezone *tz)
   if (NULL != tv)
   {
     GetSystemTimeAsFileTime(&ft);
- 
+
     tmpres |= ft.dwHighDateTime;
     tmpres <<= 32;
     tmpres |= ft.dwLowDateTime;
- 
+
     /*converting file time to unix epoch*/
     tmpres /= 10;  /*convert into microseconds*/
     tmpres -= DELTA_EPOCH_IN_MICROSECS; 
     tv->tv_sec = (long)(tmpres / 1000000UL);
     tv->tv_usec = (long)(tmpres % 1000000UL);
   }
- 
+
   if (NULL != tz)
   {
     if (!tzflag)
