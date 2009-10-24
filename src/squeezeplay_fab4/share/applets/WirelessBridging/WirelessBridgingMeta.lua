164b01da171e4c50cb904ed18ae05d640092d733
Felix Mueller <fmueller@logitech.com>
2009-10-24 11:31:04 +0000

    Description: Disable unfinished wireless bridging applet.
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.5/trunk/squeezeplay@7930 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay_fab4/share/applets/WirelessBridging/WirelessBridgingMeta.lua
+++ src/squeezeplay_fab4/share/applets/WirelessBridging/WirelessBridgingMeta.lua
@@ -17,7 +17,21 @@ end
 
 
 function registerApplet(meta)
+
+--[[
+
+Disable unfinished applet - known issues (see also bug 11150):
+
+a) Dynamically added routes for clients are lost when Fab4 is power-cycled
+b) If a client is removed, the route isn't removed automatically
+c) A client cannot use a static IP address as the route is then missing
+d) Network setup changes, i.e. using wired instead of wireless does not disable bridging
+e) UDP packets are not bridged / forwarded
+
 	jiveMain:addItem(meta:menuItem('appletWirelessBridging', 'networkSettings', "Wireless Bridging", function(applet, ...) applet:settingsShow(...) end))
+
+--]]
+
 end
 
 
