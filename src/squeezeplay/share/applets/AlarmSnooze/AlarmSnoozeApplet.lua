4656682c727930babb2d82ee0a3121b8566f053d
Ben Klaas <bklaas@logitech.com>
2009-12-21 22:03:12 +0000

    Description: when radio is controlling another player and local alarm fires, more intelligently change the controlled player back to the radio do not use self.player in AlarmSnooze as it is a confusing object in this applet. Instead use self.localPlayer where needed.
    Bug: 14870
    
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.4/trunk/squeezeplay@8245 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/share/applets/AlarmSnooze/AlarmSnoozeApplet.lua
+++ src/squeezeplay/share/applets/AlarmSnooze/AlarmSnoozeApplet.lua
@@ -33,8 +33,6 @@ function init(self, ...)
 
 	self.alarmNext = self:getSettings()['alarmNext']
 
-	self.player = Player:getLocalPlayer()
-
 	jnt:subscribe(self)
 	self.alarmTone = "applets/AlarmSnooze/alarm.mp3"
 
@@ -55,7 +53,6 @@ function init(self, ...)
 	self.RTCAlarmTimer = Timer(timeToAlarm,
 			function ()
 				log:warn("RTC ALARM FIRING")
-				appletManager:callService("deactivateScreensaver")
 				self:openAlarmWindow()
 			end,
 			true
@@ -73,7 +70,6 @@ function notify_playerAlarmState(self, player, alarmState, alarmNext)
 
 	if player:isLocal() then
 
-		self.player = player
 		log:warn(alarmState)
 		-- store alarmNext data as epoch seconds
 		if alarmNext and alarmNext > 0 then
@@ -86,17 +82,11 @@ function notify_playerAlarmState(self, player, alarmState, alarmNext)
 		end
 
 		if alarmState == 'active' then
-			if player ~= Player:getCurrentPlayer() then
-				log:warn('alarm has fired locally, switching to local player')
-                        	appletManager:callService("setCurrentPlayer", player)
-			end
-
 			if self.alarmWindow then
 				self.alarmWindow:hide()
 				self.alarmWindow = nil
 			end
 
-			appletManager:callService("deactivateScreensaver")
 			log:info('open alarm window')
 			self:openAlarmWindow()
 
@@ -112,10 +102,17 @@ function notify_playerAlarmState(self, player, alarmState, alarmNext)
 end
 
 
+function notify_playerLoaded(self, player)
+	log:debug('notify_playerLoaded()')
+	if self.pendingAlarm then
+		self.pendingAlarm = nil
+		self:openAlarmWindow()
+	end
+end
+
+
 function notify_playerConnected(self, player)
 	if player:isLocal() then
-		self.player = player
-		self.server = self.player:getSlimServer()
 
 		if self.fallbackRunning then
 			log:warn('stop the audio on currently running RTC alarm')
@@ -156,16 +153,28 @@ function openAlarmWindow(self)
 
 	log:warn('openAlarmWindow()')
 
+	-- if radio is controlling a different player, switch to the local player (if connected)
+	-- and then have notify_playerLoaded then refire openAlarmWindow() for the pending alarm notice
+	local localPlayer   = Player:getLocalPlayer()
+	local currentPlayer = Player:getCurrentPlayer()
+
+	if currentPlayer ~= localPlayer and localPlayer:isConnected() then
+		log:warn('alarm has fired locally, switching squeezeplay control to connected local player')
+		self.pendingAlarm = true
+		appletManager:callService("setCurrentPlayer", localPlayer)
+		return
+	end
+
+	self.localPlayer = localPlayer
+	appletManager:callService("deactivateScreensaver")
+
 	-- this method is called when the alarm time is hit
 	-- when the alarm time is it, unset the wakeup mcu time
 	self:_setWakeupTime('none')
 
-	if not self.player then
-		self.player = Player:getLocalPlayer()
-		if not self.player then
-			log:warn('cannot play an alarm without a player')
-			return
-		end
+	if not self.localPlayer then
+		log:warn('cannot play an alarm without a player')
+		return
 	end
 	if self.alarmWindow then
 		return
@@ -173,9 +182,9 @@ function openAlarmWindow(self)
 
 	local window = Window('alarm_popup', self:string('ALARM_SNOOZE_ALARM'))
 
-	if not self.player:isConnected() then
+	if not self.localPlayer:isConnected() then
 		log:info('activate RTC alarm')
-		self.player:playFileInLoop(self.alarmTone)
+		self.localPlayer:playFileInLoop(self.alarmTone)
 		decode:audioGain(4096, 4096)
 		self.fallbackRunning = true
 	end
@@ -244,8 +253,8 @@ function _updateTime(self)
 end
 
 function _alarmOff(self)
-	if self.player:isConnected() then
-		self.player:stopAlarm()
+	if self.localPlayer:isConnected() then
+		self.localPlayer:stopAlarm()
 	else
 		decode:stop()
 		iconbar:setAlarm('OFF')
@@ -299,8 +308,8 @@ end
 
 
 function _alarmSnooze(self)
-	if self.player:isConnected() then
-		self.player:snooze()
+	if self.localPlayer:isConnected() then
+		self.localPlayer:snooze()
 	else
 		-- stop playback
 		decode:stop() 
