c47cf3699dee5babe77bc599c13b17b0d5d27166
Adrian Smith <triode1@btinternet.com>
2010-01-30 19:21:36 +0000

    Description: logging to understand reason for labels not being displayed and returning a null surface here (most likely cause is that they result in an SDL surface width > 16k pixels)
    Bug: 15561
    
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.5/trunk/squeezeplay@8428 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/src/ui/jive_font.c
+++ src/squeezeplay/src/ui/jive_font.c
@@ -245,6 +245,10 @@ static SDL_Surface *draw_ttf_font(JiveFont *font, Uint32 color, const char *str)
 
 	srf = TTF_RenderUTF8_Blended(font->ttf, str, clr);
 
+	if (!srf) {
+		LOG_ERROR(log_ui_draw, "render returned error: %s for string: %s\n", TTF_GetError(), str);
+	}
+
 #if 0
 	// draw text bounding box for debugging
 	if (srf) {
