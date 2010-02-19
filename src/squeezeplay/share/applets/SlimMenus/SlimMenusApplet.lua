0734683b8760e9e30702d8cb9b0c3784f4f90f34
Ben Klaas <bklaas@logitech.com>
2010-02-19 18:24:09 +0000

    Description: revert earlier change. Turn Off Player will not be a menu item for system's with soft power availability. Bug: 15579
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.5/trunk/squeezeplay@8535 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/share/applets/SlimMenus/SlimMenusApplet.lua
+++ src/squeezeplay/share/applets/SlimMenus/SlimMenusApplet.lua
@@ -508,8 +508,8 @@ local function _menuSink(self, isCurrentServer, server)
 
 			if not item.id then
 				log:info("no id for menu item: ", item.text)
-			elseif item.id == "playerpower" and System:hasSoftPower() and System:getMachine() ~= 'squeezeplay' and _player:isLocal() then
-				--ignore for local player, playerpower no longer shown to users since we use power button, unless this is a device without a power button or is controlling another
+			elseif item.id == "playerpower" and System:hasSoftPower() and System:getMachine() ~= 'squeezeplay' then
+				--ignore, playerpower no longer shown to users since we use power button, unless this is a device without a power button
 			elseif item.id == "settingsPIN" then
 				--ignore, pin no longer shown to users since we use user/pass now
 			elseif item.id == "settingsPlayerNameChange" and not isCurrentServer then
