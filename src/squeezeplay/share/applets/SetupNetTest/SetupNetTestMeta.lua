36d53b438ac8b6818e30c832dda62b0d073874c8
Adrian Smith <triode1@btinternet.com>
2010-03-07 21:51:16 +0000

    Description: add IR and touch support for NetTest so it can be used on Touch, could still do with layout improvements
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.5/trunk/squeezeplay@8637 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/share/applets/SetupNetTest/SetupNetTestMeta.lua
+++ src/squeezeplay/share/applets/SetupNetTest/SetupNetTestMeta.lua
@@ -23,30 +23,29 @@ function jiveVersion(self)
 	return 1, 1
 end
 
-
-function registerApplet(meta)
-	jnt:subscribe(meta)
-
-	meta.menu = meta:menuItem('appletSetupNetTest', 'hidden', meta:string('SETUPNETTEST'), function(applet, ...) applet:open(...) end, 100)
-	-- add the menu item to homeMenu but 'hidden', or else the playerCurrent notification method below quits happening (not sure why this is the case, but it is)
-	-- item will be made visible by moving it to advancedSettings in the playerCurrent notification below
-	jiveMain:addItem(meta.menu)
+function registerApplet(self)
+	jnt:subscribe(self)
+	self.menu = self:menuItem('appletSetupNetTest', 'hidden', self:string("SETUPNETTEST"), function(applet, ...) applet:open(...) end, 100)
+	-- to begin with, we place this in the 'hidden' node
+	jiveMain:addItem(self.menu)
 end
 
--- hide menu item when player goes away
-function notify_playerDelete(meta, player)
-	jiveMain:removeItemFromNode(meta.menu, 'advancedSettings')
+function notify_serverConnected(self, server)
+	self:_checkServer()
 end
 
-function notify_playerCurrent(meta, player)
-	-- don't show this item if the player object: 
-	--   a. doesn't exist
-	--   b. isn't connected to a server (now possible)
-	--   c. connected to SN
-	if player == nil or not player:getSlimServer() or ( player:getSlimServer() and player:getSlimServer():isSqueezeNetwork() ) then
-		jiveMain:removeItemFromNode(meta.menu, 'advancedSettings')
-	else
-		jiveMain:setCustomNode('appletSetupNetTest', 'advancedSettings')
+function notify_serverDisconnected(self, server)
+	self:_checkServer()
+end
+
+function _checkServer(self)
+	for _, server in appletManager:callService("iterateSqueezeCenters") do
+		if server:isConnected() and not server:isSqueezeNetwork() then
+			-- found a local server - show menu entry
+			jiveMain:setCustomNode('appletSetupNetTest', 'advancedSettings')
+			return
+		end
 	end
+	-- hide menu entry
+	jiveMain:removeItemFromNode(self.menu, 'advancedSettings')
 end
-
