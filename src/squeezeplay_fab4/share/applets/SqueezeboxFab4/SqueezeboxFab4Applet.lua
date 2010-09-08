e135405f614cea22edd570a2651df29a55c8498b
Felix Mueller <fmueller@logitech.com>
2010-09-08 11:58:35 +0000

    Description: Add missing iface parameter.
    Bug: 16408
    
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.6/trunk/squeezeplay@9100 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay_fab4/share/applets/SqueezeboxFab4/SqueezeboxFab4Applet.lua
+++ src/squeezeplay_fab4/share/applets/SqueezeboxFab4/SqueezeboxFab4Applet.lua
@@ -531,12 +531,12 @@ local function _updateWirelessDone(self, iface, success)
 	if iface:isWireless() then
 		if success then
 			local percentage, quality = iface:getSignalStrength()
-			iconbar:setWirelessSignal(quality ~= nil and quality or "ERROR")
+			iconbar:setWirelessSignal((quality ~= nil and quality or "ERROR"), iface)
 			if player then
 				player:setSignalStrength(percentage)
 			end
 		else		
-			iconbar:setWirelessSignal("ERROR")
+			iconbar:setWirelessSignal("ERROR", iface)
 			if player then
 				player:setSignalStrength(nil)
 			end
@@ -544,9 +544,9 @@ local function _updateWirelessDone(self, iface, success)
 	-- wired
 	else
 		if success then
-			iconbar:setWirelessSignal("ETHERNET")
+			iconbar:setWirelessSignal("ETHERNET", iface)
 		else
-			iconbar:setWirelessSignal("ETHERNET_ERROR")
+			iconbar:setWirelessSignal("ETHERNET_ERROR", iface)
 		end
 		if player then
 			player:setSignalStrength(nil)
