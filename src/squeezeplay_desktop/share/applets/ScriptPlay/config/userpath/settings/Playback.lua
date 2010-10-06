0b6a61c541ab17a4dbb91dd5e57fde0a58ec9e5e
Alan Young <ayoung1@logitech.com>
2010-10-06 06:42:56 +0000

    task : SN test harness Include a playerInit clause to ensure that the player actually connects to SN on startup.
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.6/trunk/squeezeplay@9150 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay_desktop/share/applets/ScriptPlay/config/userpath/settings/Playback.lua
+++ src/squeezeplay_desktop/share/applets/ScriptPlay/config/userpath/settings/Playback.lua
@@ -1,1 +1,1 @@
-settings = {enableAudio=1,powerState="on",volume=40,captureVolume=40,squeezeNetwork=true,}
+settings = {enableAudio=1,powerState="on",volume=40,captureVolume=40,playerInit={name="SqueezePlay",model="squeezeplay",},squeezeNetwork=true,}
