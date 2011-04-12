816e6226a82f50d8bb8b642554c259b5f986cc76
Ben Klaas <bklaas@logitech.com>
2011-04-12 20:41:10 +0000

    Description: poll for playerstatus every 10 minutes
    Bug: 15515
    
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.6/trunk/squeezeplay@9418 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/share/jive/slim/Player.lua
+++ src/squeezeplay/share/jive/slim/Player.lua
@@ -989,7 +989,7 @@ function onStage(self)
 	self.slimServer.comet:startBatch()
 	
 	-- subscribe to player status updates
-	local cmd = { 'status', '-', 10, 'menu:menu', 'useContextMenu:1', 'subscribe:0' }
+	local cmd = { 'status', '-', 10, 'menu:menu', 'useContextMenu:1', 'subscribe:600' }
 	self.slimServer.comet:subscribe(
 		'/slim/playerstatus/' .. self.id,
 		_getSink(self, cmd),
