e54dfb30b1ab94ff351cfb61d8a2fda246eb82b0
Adrian Smith <triode1@btinternet.com>
2011-04-16 08:54:58 +0000

    Description: don't switch to now playing screen on playlist update if screensavers are disabled for currently displayed window
    Bug: 17156
    
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.6/trunk/squeezeplay@9426 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- src/squeezeplay/share/applets/SlimBrowser/SlimBrowserApplet.lua
+++ src/squeezeplay/share/applets/SlimBrowser/SlimBrowserApplet.lua
@@ -3646,7 +3646,10 @@ function notify_playerPlaylistChange(self, player)
 		return
 	-- make sure we have step.window replace emptyStep.window when there are tracks and emptyStep exists
 	elseif playlistSize and emptyStep then
-		_goNowPlaying(nil, true)
+		-- only move into NowPlaying if screensaver is allowed
+		if Window:getTopNonTransientWindow():canActivateScreensaver() then
+			_goNowPlaying(nil, true)
+		end
 		if emptyStep.window then
 			emptyStep.window:hide()
 		end
