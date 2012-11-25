70d700f10ea75329aab17df5c5cf8ed077bab3bb
Ralph Irving <ralph_irving@hotmail.com>
2012-11-25 21:56:03 +0000

    Moved strnicmp from squeezeplay platform ui files to portaudio v18 decoder.

--- src/squeezeplay/src/audio/decode/decode_portaudio_v18.c
+++ src/squeezeplay/src/audio/decode/decode_portaudio_v18.c
@@ -99,6 +99,20 @@ static void finished(void *userData) {
 	}
 }
 
+int strnicmp(const char *s1, const char *s2, size_t n)
+{
+	if (n == 0)
+		return 0;
+	do {
+		if (toupper(*s1) != toupper(*s2++))
+			return toupper(*(unsigned const char *)s1) - toupper(*(unsigned const char *)--s2);
+		if (*s1++ == 0)
+			break;
+	} while (--n != 0);
+
+	return 0;
+}
+
 /*
  * This function is called by portaudio when the stream is active to request
  * audio samples
