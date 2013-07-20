3b1faae86c8660a5bd1a6e6a9fe8f0e5a337fda5
Ralph Irving <ralph_irving@hotmail.com>
2013-07-20 17:45:36 +0000

    The author of the code probably meant to dereference the pointer hd in order to get the size of the structure rather than the size of the pointer itself. Thanks dsdreamer.

--- src/squeezeplay/src/jive_debug.c
+++ src/squeezeplay/src/jive_debug.c
@@ -97,7 +97,7 @@ static int jiveL_perfhook(lua_State *L) {
 	hd = lua_newuserdata(L, sizeof(struct perf_hook_data));
 	lua_settable(L, LUA_REGISTRYINDEX);
 
-	memset(hd, 0, sizeof(hd));
+	memset(hd, 0, sizeof(*hd));
 	hd->hook_threshold = (lua_tointeger(L, 1) * CLOCKS_PER_SEC) / 1000;
 	hd->kill_threshold = (luaL_optinteger(L, 2, 0) * CLOCKS_PER_SEC) / 1000;
 
