61404727fffd4ec0cf5d36b4d60787c9453c4383
Ralph Irving <ralph_irving@hotmail.com>
2013-08-21 13:50:11 +0000

    Fix libsdl missing SDL_VIDEORESIZE events when resizing in both directions simultaneously. Increase squeezeplay win32 revision to 278.

--- src/SDL-1.2.15/src/video/x11/SDL_x11events_c.h
+++ src/SDL-1.2.15/src/video/x11/SDL_x11events_c.h
@@ -27,8 +27,3 @@
 extern void X11_InitOSKeymap(_THIS);
 extern void X11_PumpEvents(_THIS);
 extern void X11_SetKeyboardState(Display *display, const char *key_vec);
-
-/* Variables to be exported */
-extern int X11_PendingConfigureNotifyWidth;
-extern int X11_PendingConfigureNotifyHeight;
-
