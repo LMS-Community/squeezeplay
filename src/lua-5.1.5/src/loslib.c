8df2076b74ef6882c721f21232c4cce003af920c
Ralph Irving <ralph_irving@hotmail.com>
2018-01-07 13:44:53 +0000

    Fix lua 5.1.5 LUA_NUMBER equals float compiler warning.

--- src/lua-5.1.5/src/loslib.c
+++ src/lua-5.1.5/src/loslib.c
@@ -193,7 +193,7 @@ static int os_time (lua_State *L) {
       * Note: there are some known processors, where 'time_t' is i.e. 'double',
       *       in such cases the (original) way of pushing a number is better.
      */
-   #ifdef LUA_NUMBER==float
+   #if LUA_NUMBER==float
      lua_pushinteger(L, t);
    #else
       lua_pushnumber(L, (lua_Number)t);
@@ -204,7 +204,7 @@ static int os_time (lua_State *L) {
 
 
 static int os_difftime (lua_State *L) {
-#ifdef LUA_NUMBER==float
+#if LUA_NUMBER==float
     lua_pushinteger(L, difftime((time_t)(luaL_checkinteger(L, 1)),
                                 (time_t)(luaL_optinteger(L, 2, 0))));
 #else
